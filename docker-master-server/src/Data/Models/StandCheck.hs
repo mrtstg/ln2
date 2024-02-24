0a9fb9c279060352c3590a551976158993ea7b37
mrtstg <russiankurwacsgo@gmail.com>
2024-02-24 12:26:18 +0300

    fix: changed data type to matching with agent

--- docker-master-server/src/Data/Models/StandCheck.hs
+++ docker-master-server/src/Data/Models/StandCheck.hs
@@ -15,18 +15,29 @@ data StandCheckStage = CopyFile
   , getStageFilePath    :: !FilePath
   }
   | ExecuteCommand
-  { getStageContainer    :: !ContainerName
-  , getStageCommand      :: !T.Text
-  , getStandRecordStdout :: !Bool
+  { getStageContainer       :: !ContainerName
+  , getStageCommand         :: !T.Text
+  , getStandRecordStdout    :: !Bool
+  , getStandFormattedOutput :: !Bool
   } deriving (Show, Eq, Generic)
 
 instance ToJSON StandCheckStage where
   toJSON (CopyFile container file path) = object ["action" .= String "copy", "container" .= container, "fileContent" .= file, "filePath" .= path]
-  toJSON (ExecuteCommand container command recordStdout) = object ["action" .= String "command", "container" .= container, "command" .= command, "recordStdout" .= recordStdout]
+  toJSON (ExecuteCommand container command recordStdout formatOut) = object
+    [ "action" .= String "command"
+    , "container" .= container
+    , "command" .= command
+    , "recordStdout" .= recordStdout
+    , "formatOutput" .= formatOut
+    ]
 
 instance FromJSON StandCheckStage where
   parseJSON = withObject "StandCheckStage" $ \v -> case K.lookup "action" v of
     Nothing -> fail "No action specified"
     (Just (String "copy")) -> CopyFile <$> v .: "container" <*> v .: "fileContent" <*> v .: "filePath"
-    (Just (String "command")) -> ExecuteCommand <$> v .: "container" <*> v .: "command" <*> v .: "recordStdout"
+    (Just (String "command")) -> ExecuteCommand
+      <$> v .: "container"
+      <*> v .: "command"
+      <*> v .:? "recordStdout" .!= True
+      <*> v .:? "formatOutput" .!= False
     _anyOther -> fail "Wrong action type, excepted string!"
