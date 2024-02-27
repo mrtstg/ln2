89d25fb06828355ddfb0963c08dfc5b17b3ea57c
mrtstg <russiankurwacsgo@gmail.com>
2024-02-27 22:01:24 +0300

    feat: routes to get user info and delete users

--- auth-service/src/App/Commands.hs
+++ auth-service/src/App/Commands.hs
@@ -16,6 +16,7 @@ import           Database.Persist.Postgresql
 import           Foundation
 import           Handlers.Auth
 import           Handlers.User
+import           Handlers.UserDetail
 import           Handlers.Validate
 import           System.Exit
 import           Utils
