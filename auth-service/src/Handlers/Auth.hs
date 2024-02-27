73fd36d99448955fe4b57b5244a108392a572145
mrtstg <russiankurwacsgo@gmail.com>
2024-02-27 20:07:48 +0300

    feat: added basic auth route and module for redis operations

--- auth-service/src/Handlers/Auth.hs
+++ auth-service/src/Handlers/Auth.hs
@@ -1,7 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 module Handlers.Auth (postAuthRouteR) where
 
+import           Data.Aeson
+import qualified Data.ByteString.Char8       as BS
+import           Data.Models.UserAuthRequest
+import qualified Data.Text                   as T
+import           Data.UUID.V4
+import           Database.Persist
 import           Foundation
+import           Network.HTTP.Types
+import           Redis
+import           Utils                       (sha256Text)
 import           Yesod.Core
+import           Yesod.Persist
 
 postAuthRouteR :: Handler Value
-postAuthRouteR = undefined
+postAuthRouteR = do
+  UserAuthRequest { .. } <- requireCheckJsonBody
+  let pwdHash = sha256Text getAuthRequestPassword
+  userExists <- runDB $ exists [UserLogin ==. getAuthRequestLogin, UserPasswordHash ==. pwdHash]
+  if not userExists then sendStatusJSON forbidden403 $ object [ "error" .= String "Invalid credentials" ] else do
+    authToken <- liftIO nextRandom >>= return . show
+    App { redisPool = redisConn } <- getYesod
+    liftIO $ rewriteAuthToken redisConn getAuthRequestLogin authToken
+    sendStatusJSON status200 $ object [ "token" .= (String . T.pack) authToken ]
