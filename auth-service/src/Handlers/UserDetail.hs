b4420218856013ae54cd41cf5366c6fdac13bd6a
mrtstg <russiankurwacsgo@gmail.com>
2024-03-08 21:07:49 +0300

    feat: added route for getting user by id

--- auth-service/src/Handlers/UserDetail.hs
+++ auth-service/src/Handlers/UserDetail.hs
@@ -1,6 +1,11 @@
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
-module Handlers.UserDetail (getUserIdDetailR, deleteUserIdDetailR) where
+module Handlers.UserDetail
+  ( getUserNameDetailR
+  , deleteUserNameDetailR
+  , getUserIdDetailR
+  , deleteUserIdDetailR
+  ) where
 
 import           Crud                        (getUserAssignedRoles)
 import           Data.Aeson
@@ -15,8 +20,32 @@ import           Yesod.Persist
 notFoundErr :: Value
 notFoundErr = object [ "error" .= String "Not found!" ]
 
-getUserIdDetailR :: Text -> Handler Value
-getUserIdDetailR userLogin' = do
+getUserIdDetailR :: UserId -> Handler Value
+getUserIdDetailR uId' = do
+  userExists <- runDB $ exists [ UserId ==. uId' ]
+  if not userExists then sendStatusJSON status404 notFoundErr else do
+    userObj <- runDB $ selectFirst [ UserId ==. uId' ] []
+    case userObj of
+      Nothing  -> sendStatusJSON status404 notFoundErr
+      (Just e@(Entity uId _)) -> do
+        userRoles <- runDB $ getUserAssignedRoles uId
+        sendStatusJSON status200 $ userDetailsFromModel e userRoles
+
+deleteUserIdDetailR :: UserId -> Handler Value
+deleteUserIdDetailR uId' = do
+  userExists <- runDB $ exists [ UserId ==. uId' ]
+  if not userExists then sendStatusJSON status404 notFoundErr else do
+    userObj <- runDB $ selectFirst [ UserId ==. uId' ] []
+    case userObj of
+      Nothing               -> sendStatusJSON status404 notFoundErr
+      (Just (Entity uId _)) -> do
+        runDB $ do
+          deleteWhere [ RoleAssignUser ==. uId ]
+          delete uId
+        sendResponseStatus status204 ()
+
+getUserNameDetailR :: Text -> Handler Value
+getUserNameDetailR userLogin' = do
   userExists <- runDB $ exists [ UserLogin ==. userLogin' ]
   if not userExists then sendStatusJSON status404 notFoundErr else do
     userObj <- runDB $ selectFirst [ UserLogin ==. userLogin' ] []
@@ -26,8 +55,8 @@ getUserIdDetailR userLogin' = do
         userRoles <- runDB $ getUserAssignedRoles uId
         sendStatusJSON status200 $ userDetailsFromModel e userRoles
 
-deleteUserIdDetailR :: Text -> Handler Value
-deleteUserIdDetailR userLogin' = do
+deleteUserNameDetailR :: Text -> Handler Value
+deleteUserNameDetailR userLogin' = do
   userExists <- runDB $ exists [ UserLogin ==. userLogin' ]
   if not userExists then sendStatusJSON status404 notFoundErr else do
     userObj <- runDB $ selectFirst [ UserLogin ==. userLogin' ] []
