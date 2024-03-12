03323a55fa831201780865f9c655022b37db7828
mrtstg <russiankurwacsgo@gmail.com>
2024-03-12 20:58:58 +0300

    feat: added courses api gateways

---
+++ main-site/src/Crud/Course.hs
@@ -0,0 +1,69 @@
+module Crud.Course
+  ( generateCourseMembersGroup
+  , generateCourseAdminsGroup
+  , createCourse
+  , deleteCourse
+  ) where
+
+import           Api.Role
+import           Control.Monad      (unless)
+import           Data.Models.Course
+import           Data.Models.Role
+import           Data.Text          (pack)
+import           Data.Time.Clock
+import           Data.UUID.V4       (nextRandom)
+import           Database.Persist
+import           Foundation
+import           Yesod.Core
+import           Yesod.Persist
+
+generateCourseAdminsGroup :: String -> String
+generateCourseAdminsGroup uid = "members-" <> uid
+
+generateCourseMembersGroup :: String -> String
+generateCourseMembersGroup uid = "admins-" <> uid
+
+-- TODO: unify interface
+createCourse :: String -> Int -> CourseCreate -> Handler (Maybe (Entity Course))
+createCourse userName uId (CourseCreate courseName) = do
+  courseUUID' <- liftIO nextRandom
+  let courseUUID = show courseUUID'
+  createTime <- liftIO getCurrentTime
+  courseEntity' <- runDB $ do
+    insertKey (CourseKey courseUUID) (Course (pack courseName) uId createTime)
+    get $ CourseKey courseUUID
+  case courseEntity' of
+    Nothing                    -> return Nothing
+    (Just e) -> do
+      let roleName = generateCourseMembersGroup courseUUID
+      let adminsRoleName = generateCourseAdminsGroup courseUUID
+      createRoleRes <- liftIO $ createRole' roleName ("Участники курса " <> courseUUID)
+      adminsRoleRes <- liftIO $ createRole' adminsRoleName ("Администраторы курса " <> courseUUID)
+      case (createRoleRes, adminsRoleRes) of
+        (v, v') | v `elem` [NoAuthURL, Api.Role.InternalError] || v' `elem` [NoAuthURL, Api.Role.InternalError] -> do
+          runDB $ delete (CourseKey courseUUID)
+          _ <- liftIO $ deleteRole' roleName
+          _ <- liftIO $ deleteRole' adminsRoleName
+          return Nothing
+        _roleExistsSomeway -> do
+          creatorAssigned <- liftIO $ isRoleAssigned'' userName roleName
+          creatorAssigned' <- liftIO $ isRoleAssigned'' userName adminsRoleName
+          unless creatorAssigned $ do
+            _ <- liftIO $ assignRole' userName roleName -- TODO: assign handle
+            return ()
+          unless creatorAssigned' $ do
+            _ <- liftIO $ assignRole' userName roleName
+            return ()
+          return $ Just (Entity (CourseKey courseUUID) e)
+
+deleteCourse :: String -> Handler Bool
+deleteCourse courseUUID = do
+  let roleName = generateCourseMembersGroup courseUUID
+  let adminsRoleName = generateCourseAdminsGroup courseUUID
+  courseExists <- runDB $ exists [CourseId ==. CourseKey courseUUID]
+  if courseExists then do
+    _ <- liftIO $ deleteRole' roleName
+    _ <- liftIO $ deleteRole' adminsRoleName
+    runDB $ delete (CourseKey courseUUID)
+    return True
+  else return False
---
+++ main-site/src/Handlers/CoursePage.hs
@@ -0,0 +1,7 @@
+module Handlers.CoursePage (getCourseR) where
+
+import           Foundation
+import           Yesod.Core
+
+getCourseR :: Key Course -> Handler Value
+getCourseR = undefined
