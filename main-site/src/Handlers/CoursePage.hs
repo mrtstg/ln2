3c6852e0827b3f9da2c92873668e98fe390ad8f7
mrtstg <russiankurwacsgo@gmail.com>
2024-04-03 21:36:39 +0300

    refactor: moved out validation function into another function

--- main-site/src/Handlers/CoursePage.hs
+++ main-site/src/Handlers/CoursePage.hs
@@ -2,11 +2,12 @@
 {-# LANGUAGE QuasiQuotes       #-}
 {-# LANGUAGE RecordWildCards   #-}
 {-# LANGUAGE TypeFamilies      #-}
-module Handlers.CoursePage (getCourseR) where
+module Handlers.CoursePage (getCourseR, getAdminCourseR) where
 
 import           Api.Login
 import           Crud.Course
 import           Crud.CourseTask
+import           Data.Models.Role
 import           Data.Models.User
 import           Data.Text        (pack, unpack)
 import           Database.Persist
@@ -15,27 +16,39 @@ import           Handlers.Utils
 import           Yesod.Core
 import           Yesod.Persist
 
+validateCourseId :: CourseId -> UserDetails -> (String -> [RoleDetails] -> Bool) -> Route App -> Handler (Entity Course)
+validateCourseId cId (UserDetails { .. }) controlF failureR = do
+  courseRes <- runDB $ selectFirst [ CourseId ==. cId ] []
+  case courseRes of
+    Nothing -> redirect failureR
+    (Just e@(Entity (CourseKey courseUUID) _)) -> do
+      let isPermited = controlF courseUUID getUserRoles
+      if not isPermited then redirect failureR else return e
+
+getAdminCourseR :: CourseId -> Handler Html
+getAdminCourseR cId = do
+  userD@(UserDetails { .. }) <- requireAuth
+  (Entity _ (Course { .. })) <- validateCourseId cId userD isUserCourseAdmin AdminCoursesR
+  defaultLayout $ do
+    [whamlet|
+      <h1 .title.is-3> #{show cId}|]
+
 -- TODO: errors screens
 getCourseR :: CourseId -> Handler Html
 getCourseR cId = do
-  (UserDetails { .. }) <- requireAuth
-  courseRes <- runDB $ selectFirst [ CourseId ==. cId ] []
-  case courseRes of
-    Nothing -> redirect CoursesR
-    (Just (Entity (CourseKey courseUUID) (Course { .. }))) -> do
-      let isMember = isUserCourseMember courseUUID getUserRoles
-      if not isMember then redirect CoursesR else do
-        pageV <- getPageNumber
-        (tasks, taskA) <- getCourseTasks cId pageV
-        acceptedTasks <- runDB $ do
-          accept' <- selectList [
-            CourseSolveAcceptionUserId ==. getUserDetailsId,
-            CourseSolveAcceptionTaskId <-. map entityKey tasks
-            ] []
-          return $ map (courseSolveAcceptionTaskId . entityVal) accept'
-        defaultLayout $ do
-          setTitle $ toHtml ("Курс: " <> unpack courseName)
-          [whamlet|
+  userD@(UserDetails { .. }) <- requireAuth
+  (Entity _ (Course { .. })) <- validateCourseId cId userD isUserCourseMember CoursesR
+  pageV <- getPageNumber
+  (tasks, taskA) <- getCourseTasks cId pageV
+  acceptedTasks <- runDB $ do
+    accept' <- selectList [
+      CourseSolveAcceptionUserId ==. getUserDetailsId,
+      CourseSolveAcceptionTaskId <-. map entityKey tasks
+      ] []
+    return $ map (courseSolveAcceptionTaskId . entityVal) accept'
+  defaultLayout $ do
+    setTitle $ toHtml ("Курс: " <> unpack courseName)
+    [whamlet|
 <div .container.pt-2.py-3>
   <h1 .title.pb-3> #{ courseName }
   <div .columns.is-multiline>
