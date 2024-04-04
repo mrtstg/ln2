60c02057b6e396437f50a120a79dbd2d4fab0e38
mrtstg <russiankurwacsgo@gmail.com>
2024-04-04 22:00:37 +0300

    feat: added basic binds for admin form

---
+++ frontend/admin-course-form/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) =>  {
+  return {
+    define: {
+      API_URL: JSON.stringify(command === 'serve' ? "http://localhost" : "http://production")
+    },
+    plugins: [svelte()]
+  }
+})
