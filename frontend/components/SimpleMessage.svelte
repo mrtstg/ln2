bd8be1e1a9d346b7dfa61a9ffbe99d849262e8ee
mrtstg <russiankurwacsgo@gmail.com>
2024-04-07 13:25:22 +0300

    feat: added new basic ui components

---
+++ frontend/components/SimpleMessage.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let title: string;
+  export let description: string;
+  export let additionalStyle: string;
+</script>
+
+<article class="message {additionalStyle}">
+  <div class="message-header">
+    <p> { title } </p>
+  </div>
+  <div class="message-body">
+    { description }
+  </div>
+</article>
