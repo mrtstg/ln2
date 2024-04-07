218fc662e97f578536095bfb0a4962db25bcfb4f
mrtstg <russiankurwacsgo@gmail.com>
2024-04-07 13:26:04 +0300

    feat: added basic check scheme creation

--- frontend/admin-course-form/src/App.svelte
+++ frontend/admin-course-form/src/App.svelte
@@ -1,36 +1,74 @@
 <script lang="ts">
+  import * as Icon from 'flowbite-svelte-icons'
   import { ApiClient } from "../../api/client"
+  import type { ContainerSummary } from "../../api/types";
+  import DangerMessage from "../../components/DangerMessage.svelte"
+  import CheckStageWidget from "../../components/CheckStage.svelte"
+  import SuccessMessage from "../../components/SuccessMessage.svelte"
+  import { type StageType, type CheckStage, defaultCheckStageData } from "../../api/check_stage" 
+  import { stageTypeList } from "../../api/check_stage"
 
   const url = API_URL;
   const api = new ApiClient(url)
 
+  let selectedStand: string;
+  $: selectedStand = ''
+
+  let stages: CheckStage[]
+  $: stages = []
+
+  const addStage = () => {
+    stages = [...stages, {type: stageTypeList[0], data: defaultCheckStageData(stageTypeList[0])}]
+  }
+
   let standsPromise = api.getStands()
+  let containersPromise: Promise<Array<ContainerSummary>> | null = null
 </script>
 
 <div class="container p-5">
   {#await standsPromise}
-    <article class="message is-success">
-      <div class="message-header">
-        <p> Загружаем данные... </p>
-      </div>
-    </article>
+    <SuccessMessage title="Ожидайте..." description="Загружаем данные..." additionalStyle="is-fullwidth"/>
   {:then standsData}
-    <div class="select">
-      <select>
-        <option disabled> Выберите стенд </option>
-        {#each standsData as standName}
-          <option value={standName}> { standName } </option>
-        {/each}
-      </select>
+    <h2 class="title is-3"> Схема проверки </h2>
+    <h3 class="title is-4"> Выберите стенд </h3>
+    <div class="field">
+      <div class="control">
+        <div class="select">
+          <select bind:value={selectedStand} on:change={() => containersPromise = api.getStandContainers(selectedStand)}>
+            <option disabled> Выберите стенд </option>
+            {#each standsData as standName}
+              <option value={standName}> { standName } </option>
+            {/each}
+          </select>
+        </div>
+      </div>
     </div>
+
+    {#if containersPromise != null}
+      {#await containersPromise}
+        <SuccessMessage title="Ожидайте..." description="Загружаем данные..."/>
+      {:then containersData}
+        <div class="is-flex">
+          <h3 class="title is-4 pr-3"> Этапы проверки </h3>
+          <div>
+            <button class="button is-link" on:click={addStage}>
+              <span class="icon is-large">
+                <Icon.PlusOutline/>
+              </span>
+            </button>
+          </div>
+        </div>
+        {#each stages as stage, stageIndex}
+          <CheckStageWidget data={stage} containers={containersData.map(v => v.name)} updateCallback={async (v) => { stages[stageIndex] = v }}/>
+        {/each}
+        <button on:click={addStage} class="is-link button is-fullwidth"> Добавить </button>
+        <br>
+        { JSON.stringify(stages) }
+      {:catch error}
+        <DangerMessage title="Ошибка!" description="Не удалось загрузить данные стенда."/>
+      {/await}
+    {/if}
   {:catch error}
-    <article class="message is-danger">
-      <div class="message-header">
-        <p> Ошибка! </p>
-      </div>
-      <div class="message-body">
-        Не удалось получить данные о доступных стендах.
-      </div>
-    </article>
+    <DangerMessage title="Ошибка!" description="Не удалось получить данные о доступных стеднах."/>
   {/await}
 </div>
---
+++ frontend/components/CheckStage.svelte
@@ -0,0 +1,120 @@
+<script lang="ts">
+  import type { CheckStage, StageData } from "../api/check_stage"
+  import { StageType } from "../api/check_stage"
+  import { stageTypeList, defaultCheckStageData, checkStageName } from "../api/check_stage"
+  import DangerMessage from "../components/DangerMessage.svelte"
+  import SelectField from "../components/SelectField.svelte"
+
+  export let data: CheckStage
+  export let containers: string[]
+  export let updateCallback: (data: CheckStage) => Promise<void>
+
+  let selectedActionIndex: number
+  $: selectedActionIndex = 0
+  let selectedType: StageType
+  $: selectedType = stageTypeList[selectedActionIndex]
+  for (let i = 0; i < stageTypeList.length; i++) {
+    if (stageTypeList[i] == data.type) {
+      selectedActionIndex = i
+      selectedType = stageTypeList[selectedActionIndex]
+    }
+  }
+
+  const updateCheckStage = async (v: string) => {
+    selectedActionIndex = parseInt(v)
+    data.type = stageTypeList[selectedActionIndex]
+    data.data = defaultCheckStageData(data.type)
+    await updateCallback(data)
+  }
+</script>
+
+<div class="box my-3">
+  <div class="field">
+    <label class="label"> Тип действия </label>
+    <div class="control">
+      <div class="select">
+        <select on:change={async (event) => { await updateCheckStage(event.target.value) }}>
+          {#each stageTypeList as stageType, stageIndex}
+            <option value={stageIndex}> {checkStageName(stageType)} </option>
+          {/each}
+        </select>
+      </div>
+    </div>
+  </div>
+  {#if selectedType == StageType.CopyFile }
+    <SelectField title="Контейнер выполнения" items={containers} selectCallback={async (v) => { data.data.container = v; await updateCallback(data) }}/>
+    <div class="field">
+      <label class="label"> Путь файла </label>
+      <div class="control">
+        <input class="input" type="text" placeholder="Путь файла, например /a.sql" bind:value={data.data.filePath} on:change={async () => await updateCallback(data) }>
+      </div>
+    </div>
+    <div class="field">
+      <label class="label"> Содержимое файла </label>
+      <div class="control">
+        <textarea class="textarea" bind:value={data.data.fileContent} on:change={async () => await updateCallback(data) }></textarea>
+      </div>
+    </div>
+  {:else if selectedType == StageType.ExecuteCommand }
+    <SelectField title="Контейнер выполнения" items={containers} selectCallback={async (v) => { data.data.container = v; await updateCallback(data) }}/>
+    <div class="field">
+      <label class="label"> Команда для выполнения </label>
+      <div class="control">
+        <input class="input" type="text" placeholder="Команда, например psql -f /a.sql" bind:value={data.data.command} on:change={async () => await updateCallback(data) }>
+      </div>
+    </div>
+    <label class="checkbox">
+      <input type="checkbox" bind:checked={data.data.formatOutput} on:change={async () => await updateCallback(data)}/>
+      Форматировать вывод
+    </label>
+    <div class="field">
+      <label class="label"> Записать в переменную (<i>опционально</i>)</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="Название переменной, например result" maxlength="25" bind:value={data.data.recordInto} on:change={async () => await updateCallback(data)}>
+      </div>
+    </div>
+  {:else if selectedType == StageType.CopyAnswer }
+    <SelectField title="Скопировать на" items={containers} selectCallback={async (v) => { data.data.container = v; await updateCallback(data) }}/>
+    <div class="field">
+      <label class="label"> Путь файла </label>
+      <div class="control">
+        <input class="input" type="text" placeholder="Путь для сохранения, например a.sql" bind:value={data.data.filePath} on:change={async () => await updateCallback(data)}>
+      </div>
+    </div>
+  {:else if selectedType == StageType.DeclareVariable}
+    <div class="field">
+      <label class="label"> Название </label>
+      <div class="control">
+        <input class="input" type="text" placeholder="Название переменной, например result" maxlength="25" bind:value={data.data.variableName} on:change={async () => await updateCallback(data)}>
+      </div>
+    </div>
+    <div class="field">
+      <label class="label"> Значение </label>
+      <div class="control">
+        <textarea class="textarea" bind:value={data.data.variableValue} on:change={async () => await updateCallback(data) }></textarea>
+      </div>
+    </div>
+  {:else if selectedType == StageType.CompareResults}
+    <div class="field">
+      <label class="label"> Первая переменная </label>
+      <div class="control">
+        <input class="input" type="text" placeholder="Название переменной" maxlength="25" bind:value={data.data.first} on:change={async () => await updateCallback(data)}>
+      </div>
+    </div>
+    <div class="field">
+      <label class="label"> Вторая переменная </label>
+      <div class="control">
+        <input class="input" type="text" placeholder="Название переменной" maxlength="25" bind:value={data.data.second} on:change={async () => await updateCallback(data)}>
+      </div>
+    </div>
+    <div class="field">
+      <label class="label"> Количество зачтенных баллов </label>
+      <div class="control">
+        <input class="input" type="number" placeholder="Количество баллов" bind:value={data.data.score} on:change={async () => await updateCallback(data)}>
+      </div>
+    </div>
+  {:else}
+    <DangerMessage title="Ошибка!" description="Неизвестный тип"/>
+  {/if}
+  { JSON.stringify(data) }
+</div>
