d649234037c2c2b2401ca9f57f7a8a490a1ee787
mrtstg <russiankurwacsgo@gmail.com>
2024-04-07 13:25:22 +0300

    feat: added new basic ui components

---
+++ frontend/components/SelectField.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  export let items: string[]
+  export let title: string
+  export let selectCallback: (item: string) => Promise<void>;
+
+  if (items.length > 0) {
+    selectCallback(items[0]).then()
+  }
+</script>
+
+<div class="field">
+  <label class="label"> { title } </label>
+  <div class="control">
+    <div class="select">
+      <select on:change={async (event) => { selectCallback(event.target.value) }}>
+        {#each items as item}
+          <option value={item}> { item } </option>
+        {/each}
+      </select>
+    </div>
+  </div>
+</div>
---
+++ frontend/components/SuccessMessage.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import SimpleMessage from "./SimpleMessage.svelte";
+
+  export let title: string;
+  export let description: string;
+  export let additionalStyle: string = "";
+</script>
+
+<SimpleMessage title={title} description={description} additionalStyle="is-success {additionalStyle}"/>
