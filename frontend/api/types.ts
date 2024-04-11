de8dd8b286346e1ec900adb2595ca5eb7c7916f1
mrtstg <russiankurwacsgo@gmail.com>
2024-04-11 21:39:52 +0300

    feat: new api bindings and types

--- frontend/api/types.ts
+++ frontend/api/types.ts
@@ -1,4 +1,44 @@
+import * as CheckStage from "./check_stage.ts"
+
 export type ContainerSummary = {
   image: string,
   name: string
+}
+
+export type RoleDetails = {
+  name: string,
+  displayName: string
+}
+
+export type UserDetails = {
+  id: number,
+  login: string,
+  roles: [RoleDetails]
+}
+
+export type CommonCourseDetails = {
+  author: UserDetails | null,
+  authorId: number,
+  createdAt: Date,
+  id: string,
+  name: string
+}
+
+export type CourseTaskCreate = {
+  name: string,
+  content: string,
+  order: number,
+  standIdentifier: string,
+  standActions: CheckStage.StageData[]
+}
+
+// TODO: add course field
+export type CourseTaskDetails = {
+  id: number,
+  name: string,
+  content: string,
+  order: number,
+  accepted?: Boolean,
+  standIdentifier?: string,
+  standActions?: [object]
 }
