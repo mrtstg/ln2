c592a9aa86bf2d7d3e93180dc9146a9ad8ad43aa
mrtstg <russiankurwacsgo@gmail.com>
2024-04-11 19:31:14 +0300

    fix: set api url to same domain when building

--- frontend/admin-course-form/vite.config.ts
+++ frontend/admin-course-form/vite.config.ts
@@ -5,7 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig(({ command, mode }) =>  {
   return {
     define: {
-      API_URL: JSON.stringify(command === 'serve' ? "http://localhost" : "http://production")
+      API_URL: JSON.stringify(command === 'serve' ? "http://localhost" : "")
     },
     plugins: [svelte()]
   }
