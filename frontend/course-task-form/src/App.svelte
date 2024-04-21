2031c335b7872acdb6a7b32b69bb5b1acdf6ce33
mrtstg <russiankurwacsgo@gmail.com>
2024-04-21 13:43:32 +0300

    feat: added task result loading (if its still stored)

--- frontend/course-task-form/src/App.svelte
+++ frontend/course-task-form/src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { ApiClient } from "../../api/client"
-  import type { CourseTaskSolve, CourseSolvesResponse } from "../../api/types"
+  import type { CourseTaskSolve, CourseSolvesResponse, TaskResult } from "../../api/types"
   import DangerMessage from "../../components/DangerMessage.svelte"
   import SuccessMessage from "../../components/SuccessMessage.svelte"
 
@@ -14,6 +14,8 @@
   let selectedTask: CourseTaskSolve | null
   $: selectedTask = null
   let taskSolvesPromise: Promise<CourseSolvesResponse> | null = null
+  let taskResultPromise: Promise<TaskResult | null> | null = null
+
   let tasks: Array<CourseTaskSolve>
   $: tasks = []
 
@@ -34,6 +36,16 @@
     taskSolvesPromise = taskSolvesWrapper()
   }
 
+  const selectTask = (task: CourseTaskSolve) => {
+    selectedTask = task
+    taskResultPromise = api.getTaskDetails(task.id)
+  }
+
+  const unselectTask = () => {
+    selectedTask = null
+    taskResultPromise = null
+  }
+
 </script>
 
 {#if taskSolvesPromise != null}
@@ -45,15 +57,31 @@
         <h2 class="subtitle is-4"> Решение { selectedTask.id } </h2>
         <div class="field">
           <label class="label"> Ответ пользователя </label>
-          <textarea class="textarea" rows="25" readonly>{ selectedTask.input }</textarea>
+          <textarea class="textarea" rows="{Math.min(25, selectedTask.input.split('\n').length)}" readonly>{ selectedTask.input }</textarea>
         </div>
-        <button class="button is-danger is-fullwidth" on:click={() => selectedTask = null}> Назад </button>
+        <button class="button is-danger is-fullwidth" on:click={unselectTask}> Назад </button>
+        {#if taskResultPromise != null}
+          <div class="pt-5">
+            {#await taskResultPromise}
+              <SuccessMessage title="Ожидайте..." description="Загружаем результаты проверки."/>
+            {:then result}
+              {#if result == null}
+                <DangerMessage title="Ошибка!" description="Результат проверки задания больше не хранится в системе." additionalStyle="is-fullwidth"/>
+              {:else}
+                  <h3 class="subtitle is-5"> Результаты проверки </h3>
+                  <p> Набрано баллов: { result.score } из { result.maxScore } </p>
+              {/if}
+            {:catch e}
+              <DangerMessage title="Ошибка!" description="Результат проверки задания был загружен с ошибкой." additionalStyle="is-fullwidth"/>
+            {/await}
+          </div>
+        {/if}
       </div>
     {:else}
       <div class="columns is-multiline">
         {#each tasks as task}
           <div class="column is-6">
-            <div class="card" on:click={() => selectedTask = task}>
+            <div class="card" on:click={() => selectTask(task)}>
               <header class="card-header">
                 {#if task.correct}
                   <p class="card-header-title has-text-success"> Решение { task.id } </p>
