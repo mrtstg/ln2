4070fe16e844c95893cec7d9a5f2e507d44f24b7
mrtstg <russiankurwacsgo@gmail.com>
2024-04-21 15:33:08 +0300

    feat: added new message component

---
+++ frontend/components/WarningMessage.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import SimpleMessage from "./SimpleMessage.svelte";
+
+  export let title: string;
+  export let description: string;
+  export let additionalStyle: string = "";
+</script>
+
+<SimpleMessage title={title} description={description} additionalStyle="is-warning {additionalStyle}"/>
+
