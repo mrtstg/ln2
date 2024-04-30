7ec742f13779ea3f892c1e1e6307d107b683336e
mrtstg <russiankurwacsgo@gmail.com>
2024-04-30 18:44:49 +0300

    feat: added function for getting user details by user id

--- auth-service/src/Crud.hs
+++ auth-service/src/Crud.hs
@@ -2,7 +2,11 @@
 {-# LANGUAGE GADTs                 #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies          #-}
-module Crud (getUserAssignedRoles, getUserDetailsByName) where
+module Crud
+  ( getUserAssignedRoles
+  , getUserDetailsByName
+  , getUserDetailsById
+  ) where
 
 import           Control.Monad.Trans.Reader
 import           Data.Models.User
@@ -19,6 +23,15 @@ getUserAssignedRoles
 getUserAssignedRoles uId = do
   assignations <- selectList [ RoleAssignUser ==. uId ] []
   selectList [ RoleId <-. map (\(Entity _ (RoleAssign _ rId)) -> rId) assignations ] []
+
+getUserDetailsById :: (MonadUnliftIO m) => UserId -> ReaderT SqlBackend m (Maybe UserDetails)
+getUserDetailsById uId = do
+  userObject' <- get uId
+  case userObject' of
+    Nothing -> return Nothing
+    (Just e) -> do
+      userRoles <- getUserAssignedRoles uId
+      return $ Just (userDetailsFromModel (Entity uId e) userRoles)
 
 getUserDetailsByName :: (MonadUnliftIO m) => T.Text -> ReaderT SqlBackend m (Maybe UserDetails)
 getUserDetailsByName userName = do
