858d6a9172313e252e84bc33db4ef33df08f6d77
mrtstg <russiankurwacsgo@gmail.com>
2024-05-01 22:01:49 +0300

    feat: declared app parameters for environment in structure

---
+++ docker-secondary-agent/src/structs/app_env.rs
@@ -0,0 +1,32 @@
+use std::env;
+
+#[derive(Debug, Clone)]
+pub struct AppEnvironment {
+    pub rabbit_user: String,
+    pub rabbit_pass: String,
+    pub rabbit_host: String,
+    pub rabbit_port: u16,
+    pub agent_debug: bool,
+}
+
+pub fn get_app_environment() -> AppEnvironment {
+    let rabbit_user = env::var("RABBITMQ_DEFAULT_USER").expect("Failed to get rabbitmq user!");
+    let rabbit_pass = env::var("RABBITMQ_DEFAULT_PASS").expect("Failed to get rabbitmq pass!");
+    let rabbit_host = env::var("RABBITMQ_HOST").expect("Failed to get rabbitmq host!");
+    let rabbit_port = env::var("RABBITMQ_PORT")
+        .expect("Failed to get rabbitmq port!")
+        .parse::<u16>()
+        .expect("Failed to parse rabbitmq port!");
+    let agent_debug_res = env::var("AGENT_DEBUG");
+    let agent_debug: bool = match agent_debug_res {
+        Ok(v) => v == "1",
+        Err(_) => false,
+    };
+    AppEnvironment {
+        rabbit_user,
+        rabbit_pass,
+        rabbit_host,
+        rabbit_port,
+        agent_debug,
+    }
+}
