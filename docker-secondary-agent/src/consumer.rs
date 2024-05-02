c762c32e6f6ae6ae6841302fe5e3b7be688dcff3
mrtstg <russiankurwacsgo@gmail.com>
2024-05-02 10:30:07 +0300

    feat: added logging and response exchange message send

--- docker-secondary-agent/src/consumer.rs
+++ docker-secondary-agent/src/consumer.rs
@@ -1,16 +1,49 @@
 use crate::structs::app_env::{self, AppEnvironment};
+use crate::structs::queue_response::QueueTaskResponse;
 use crate::structs::queue_task::QueueTask;
+use amqprs::channel::{self, BasicPublishArguments, Channel};
+use amqprs::BasicProperties;
 use amqprs::{channel::BasicAckArguments, consumer::AsyncConsumer};
 use async_trait::async_trait;
 use serde_json::from_str;
 
 pub struct RabbitConsumer {
     pub app_env: AppEnvironment,
+    pub publish_args: BasicPublishArguments,
 }
 
 impl RabbitConsumer {
-    pub fn new(app_env: AppEnvironment) -> Self {
-        RabbitConsumer { app_env }
+    pub fn new(app_env: AppEnvironment, publish_args: BasicPublishArguments) -> Self {
+        RabbitConsumer {
+            app_env,
+            publish_args,
+        }
+    }
+}
+
+async fn update_task_status(
+    channel: &Channel,
+    publish_args: BasicPublishArguments,
+    uuid: String,
+    status: String,
+) -> Result<(), String> {
+    let data = QueueTaskResponse {
+        uuid,
+        status,
+        result: None,
+    };
+    match serde_json::to_string(&data) {
+        Ok(string) => {
+            let content = string.as_bytes().to_vec();
+            match channel
+                .basic_publish(BasicProperties::default(), content, publish_args)
+                .await
+            {
+                Ok(_) => return Ok(()),
+                Err(e) => return Err(e.to_string()),
+            }
+        }
+        Err(e) => return Err(e.to_string()),
     }
 }
 
@@ -34,7 +67,13 @@ impl AsyncConsumer for RabbitConsumer {
                 let parse_res: Result<QueueTask, serde_json::Error> = from_str(message.as_str());
                 match parse_res {
                     Ok(queue_task) => {
-                        println!("{:?}", queue_task.stand);
+                        update_task_status(
+                            channel,
+                            self.publish_args.clone(),
+                            queue_task.uuid,
+                            "taken".to_string(),
+                        )
+                        .await;
                     }
                     Err(e) => {
                         println!("Failed to deserialize task: {}", e);
