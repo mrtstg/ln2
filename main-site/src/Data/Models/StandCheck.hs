1f00759c91d41224545b5fdb81068286ceb8a564
mrtstg <russiankurwacsgo@gmail.com>
2024-05-05 17:51:07 +0300

    feat: added new macros for postgresql work

--- main-site/src/Data/Models/StandCheck.hs
+++ main-site/src/Data/Models/StandCheck.hs
@@ -39,6 +39,15 @@ data StandCheckStage = CopyFile
   { getStageContainer :: !ContainerName
   , getCheckQuery     :: !T.Text
   , getCheckScore     :: !Int
+  }
+  | PSQLQuery
+  { getStageContainer      :: !ContainerName
+  , getCheckQuery          :: !T.Text
+  , getStandRecordVariable :: !(Maybe T.Text)
+  }
+  | PSQLAnswerQuery
+  { getStageContainer      :: !ContainerName
+  , getStandRecordVariable :: !(Maybe T.Text)
   } deriving (Show, Eq, Generic)
 
 instance ToJSON StandCheckStage where
@@ -73,6 +82,16 @@ instance ToJSON StandCheckStage where
     , "query" .= String query
     , "score" .= score
     ]
+  toJSON (PSQLQuery container query recordInto) = object
+    [ "action" .= String "psql_query_macro"
+    , "container" .= container
+    , "query" .= query
+    , "recordInto" .= recordInto
+    ]
+  toJSON (PSQLAnswerQuery container recordInto) = object
+    [ "action" .= String "psql_answer_query_macro"
+    , "container" .= container
+    , "recordInto" .= recordInto]
 
 instance FromJSON StandCheckStage where
   parseJSON = withObject "StandCheckStage" $ \v -> case K.lookup "action" v of
@@ -98,19 +117,39 @@ instance FromJSON StandCheckStage where
       <$> v .: "container"
       <*> v .: "query"
       <*> v .: "score"
+    (Just (String "psql_query_macro")) -> PSQLQuery
+      <$> v .: "container"
+      <*> v .: "query"
+      <*> v .:? "recordInto"
+    (Just (String "psql_answer_query_macro")) -> PSQLAnswerQuery
+      <$> v .: "container"
+      <*> v .:? "recordInto"
     _anyOther -> fail "Wrong action type, excepted string!"
 
--- заменяет "особый" блок ответа на копирование файла
+-- развертка макросов в простые составные блоки
 convertStandCheckList :: T.Text -> [StandCheckStage] -> [StandCheckStage]
 convertStandCheckList answer = foldr f [] where
   f :: StandCheckStage -> [StandCheckStage] -> [StandCheckStage]
   f (CopyAnswer container filePath) acc = CopyFile container answer filePath:acc
   f (PSQLExists container query score) acc  =
-    [ CopyFile container ("select (CASE WHEN EXISTS(" <> query <> ") THEN 1 ELSE 0 END);") scriptName
+    [ CopyFile container ("select (CASE WHEN EXISTS(" <> query' <> ") THEN 1 ELSE 0 END);") scriptName
     , ExecuteCommand container ("psql -f " <> scriptName' <> " --csv -t") False True (Just scriptName')
     , DeclareVariable (scriptName' <> "-correct") (String "1")
     , CompareResults (scriptName' <> "-correct") scriptName' score
     ] ++ acc where
+      query' = if T.last query == ';' then T.init query else query
       scriptName = "/" <> unsafeRandomString 15 <> ".sql"
+      scriptName' = T.pack scriptName
+  f (PSQLQuery container query recordInto) acc =
+    [ CopyFile container query scriptName
+    , ExecuteCommand container ("psql -f " <> scriptName' <> " --csv") False True recordInto
+    ] ++ acc where
+      scriptName = "/" <> unsafeRandomString 14 <> ".sql"
+      scriptName' = T.pack scriptName
+  f (PSQLAnswerQuery container recordInto) acc =
+    [ CopyFile container answer scriptName
+    , ExecuteCommand container ("psql -f " <> scriptName' <> " --csv") False True recordInto
+    ] ++ acc where
+      scriptName = "/" <> unsafeRandomString 14 <> ".sql"
       scriptName' = T.pack scriptName
   f other acc                           = other:acc
