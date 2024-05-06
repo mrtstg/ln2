ac8b530e6274da6a5d918a2ae3e41d6dc73af171
mrtstg <russiankurwacsgo@gmail.com>
2024-05-06 21:57:45 +0300

    feat: protected query API by access check

--- main-site/src/Handlers/QueryCourse.hs
+++ main-site/src/Handlers/QueryCourse.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 module Handlers.QueryCourse (getQueryCourseR) where
 
 import           Api.User           (queryUsers')
 import           Data.Maybe         (fromMaybe)
+import           Data.Models.User   (UserDetails (..))
 import           Data.Text
 import           Foundation
 import           Handlers.Utils
@@ -12,14 +14,16 @@ import           Yesod.Core
 
 getQueryCourseR :: Text -> Handler Value
 getQueryCourseR courseId = do
-  queryValue' <- lookupGetParam "query"
-  getMembers <- getBoolParameter "getMembers"
-  getAdmins <- getBoolParameter "getAdmins"
-  pageNumber <- getPageNumber
-  let queryValue = fromMaybe "" queryValue'
-  let courseMemberGroup = (Just . pack . (if getAdmins then generateCourseAdminsGroup else generateCourseMembersGroup) . unpack) courseId
-  let res'' = if getMembers then queryUsers' queryValue courseMemberGroup Nothing else queryUsers' queryValue Nothing courseMemberGroup
-  res' <- liftIO (res'' pageNumber)
-  case res' of
-    Nothing -> sendStatusJSON status500 $ object [ "error" .= String "Something went wrong!" ]
-    (Just res) -> sendStatusJSON status200 res
+  (UserDetails { .. }) <- requireApiAuth
+  if not $ isUserAnyCourseAdmin getUserRoles then sendStatusJSON status403 $ object [ "error" .= String "Unauthorized!" ] else do
+    queryValue' <- lookupGetParam "query"
+    getMembers <- getBoolParameter "getMembers"
+    getAdmins <- getBoolParameter "getAdmins"
+    pageNumber <- getPageNumber
+    let queryValue = fromMaybe "" queryValue'
+    let courseMemberGroup = (Just . pack . (if getAdmins then generateCourseAdminsGroup else generateCourseMembersGroup) . unpack) courseId
+    let res'' = if getMembers then queryUsers' queryValue courseMemberGroup Nothing else queryUsers' queryValue Nothing courseMemberGroup
+    res' <- liftIO (res'' pageNumber)
+    case res' of
+      Nothing -> sendStatusJSON status500 $ object [ "error" .= String "Something went wrong!" ]
+      (Just res) -> sendStatusJSON status200 res
