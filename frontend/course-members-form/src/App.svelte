5f4bf6647a7b6e5e91f739ad56bdcec24bf14fbb
mrtstg <russiankurwacsgo@gmail.com>
2024-05-09 12:23:21 +0300

    feat: added course members management form

---
+++ frontend/course-members-form/src/App.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import { ApiClient } from "../../api/client"
+  import UserQueryForm from "./lib/UserQueryForm.svelte"
+
+  const url = API_URL;
+  const api = new ApiClient(url)
+
+  let courseID: string | null = null
+  const parsedURL = new URL(document.URL)
+  const courseIDMatch = parsedURL.pathname.match("\/course\/(.*?)\/members")
+  if (courseIDMatch != null && courseIDMatch.length > 1) {
+    courseID = courseIDMatch[1]
+  }
+
+  const updateWindows = () => {
+    membersUpdate()
+    usersUpdate()
+  }
+
+  const userCallback = async (login: string) => {
+    await api.assignCourseMember(courseID!, login)
+    updateWindows()   
+  }
+
+  let usersUpdate: () => void
+  let membersUpdate: () => void
+
+  let hideMembers: boolean = false
+  let hideUsers: boolean = true
+</script>
+
+{#if courseID != null}
+  <div class="is-flex is-flex-direction-row is-justify-content-space-between is-align-items-center is-fullwidth my-3">
+    <h2 class="subtitle is-4"> Участники курса </h2>
+    <button class="button" on:click={() => { hideMembers = !hideMembers }}> { hideMembers ? 'Показать' : 'Скрыть' } </button>
+  </div>
+  {#if !hideMembers}
+    <UserQueryForm bind:queryWrapper={membersUpdate} apiUrl={url} courseId={courseID} actionText="Исключить" actionCallback={userCallback}/>
+  {/if}
+
+  <div class="is-flex is-flex-direction-row is-justify-content-space-between is-align-items-center is-fullwidth my-3">
+    <h2 class="subtitle is-4"> Добавить участников </h2>
+    <button class="button" on:click={() => { hideUsers = !hideUsers }}> { hideUsers ? 'Показать' : 'Скрыть' } </button>
+  </div>
+  {#if !hideUsers}
+    <UserQueryForm bind:queryWrapper={usersUpdate} apiUrl={url} courseId={courseID} getMembers={false} actionText="Добавить" actionCallback={userCallback}/>
+  {/if}
+{/if}
+
---
+++ frontend/course-members-form/src/lib/UserQueryForm.svelte
@@ -0,0 +1,87 @@
+<script lang="ts">
+  import { ApiClient } from "../../../api/client"
+  import type { UserQuery } from "../../../api/types"
+  import DangerMessage from "../../../components/DangerMessage.svelte"
+  import SuccessMessage from "../../../components/SuccessMessage.svelte"
+
+  export let apiUrl: string
+  export let courseId: string
+  export let getMembers: boolean = true
+  export let getAdmins: boolean = false
+  export let pageNumber: number = 1
+  export let actionText: string = ''
+  export let actionCallback: (login: string) => Promise<void> = async (_) => {}
+
+  let client = new ApiClient(apiUrl)
+
+  let query: string
+  $: query = ''
+
+  export const queryWrapper = () => {
+    loadCallback = client.queryCourseUsers(courseId, query, getMembers, getAdmins, pageNumber)
+  }
+
+  const cleanQuery = () => {
+    query = ''
+    queryWrapper()
+  }
+
+  const goToPage = (page: number) => {
+    pageNumber = page
+    queryWrapper()
+  }
+
+  const nextPage = () => { goToPage(pageNumber + 1) }
+  const prevPage = () => { goToPage(pageNumber - 1) }
+
+  let loadCallback: Promise<UserQuery | null> | null = null
+  queryWrapper()
+</script>
+
+<div class="is-flex is-flex-direction-row is-justify-content-space-between is-fullwidth mb-5">
+  <input maxlength="30" class="input is-flex-grow-1" type="text" placeholder="Ваш запрос" bind:value={query}>
+  <button class="button ml-3" on:click={queryWrapper}> Искать </button>
+  <button class="button ml-3" on:click={cleanQuery}> Очистить </button>
+</div>
+
+{#if loadCallback != null}
+  {#await loadCallback}
+    <SuccessMessage title="Ожидайте" description="Загружаем пользователей..." additionalStyle="is-fullwdith"/>
+  {:then queryRes}
+    {#if queryRes == null}
+      <DangerMessage title="Ошибка" description="Не удалось загрузить базу пользователей" additionalStyle="is-fullwdith"/>
+    {:else}
+      {#each queryRes.objects as userData}
+        <div class="box">
+          <div class="is-flex is-align-items-center is-flex-direction-row is-justify-content-space-between is-fullwidth">
+            <span> { userData.name } </span>
+            {#if actionText.length > 0}
+              <button class="button" on:click={async () => await actionCallback(userData.login)}> { actionText } </button>
+            {/if}
+          </div>
+        </div>
+      {/each}
+      <nav class="pagination is-centered" role="navigation">
+        <button class="pagination-previous" on:click={prevPage} disabled={pageNumber == 1}> Назад </button>
+
+        <button class="pagination-next" on:click={nextPage} disabled={pageNumber * queryRes.pageSize >= queryRes.total}> Вперед </button>
+
+        <ul class="pagination-list">
+          {#if pageNumber >= 3}
+            <li><a class="pagination-link" on:click={() => goToPage(1)}> 1 </a></li>
+            <li><span class="pagination-ellipsis">&hellip;</span></li>
+          {/if}
+          {#if pageNumber >= 2}
+            <li><a class="pagination-link" on:click={prevPage}> { pageNumber - 1 } </a></li>
+          {/if}
+          <li><a class="pagination-link is-current"> { pageNumber } </a></li>
+          {#if pageNumber * queryRes.pageSize < queryRes.total}
+            <li><a class="pagination-link" on:click={nextPage}> { pageNumber + 1} </a></li>
+          {/if}
+        </ul>
+      </nav>
+    {/if}
+  {:catch}
+    <DangerMessage title="Ошибка" description="Не удалось загрузить базу пользователей" additionalStyle="is-fullwdith"/>
+  {/await}
+{/if}
