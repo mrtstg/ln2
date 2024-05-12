7220d094b783976f5de4786e5d504cb6ebfe8d6f
mrtstg <russiankurwacsgo@gmail.com>
2024-05-12 17:00:16 +0300

    feat: added request handle functions

---
+++ db-gen-api/src/Handlers/Create.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Handlers.Create (postCheckCreateR, postConvertCreateR) where
+
+import           Control.Monad.Identity
+import           Control.Monad.Trans.Except
+import           Data.Aeson
+import           Foundation
+import           Network.HTTP.Types
+import           Render
+import           Types.Database
+import           Validate
+import           Yesod.Core
+
+validateDatabaseRequest :: Handler DatabaseData
+validateDatabaseRequest = do
+  d@(DatabaseData {}) <- requireCheckJsonBody
+  case (runIdentity . runExceptT) (validateDatabase d) of
+    (Left e)   -> sendStatusJSON status400 $ object ["error" .= show e]
+    (Right ()) -> return d
+
+postCheckCreateR :: Handler Value
+postCheckCreateR = do
+  _ <- validateDatabaseRequest
+  sendStatusJSON status204 ()
+
+postConvertCreateR :: Handler Value
+postConvertCreateR = do
+  d <- validateDatabaseRequest
+  sendStatusJSON status200 $ object ["query" .= renderDatabaseCreate d]
