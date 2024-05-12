421532ef57d40bfc2a8d1d0f2a60f4ffe72336a6
mrtstg <russiankurwacsgo@gmail.com>
2024-05-12 16:24:00 +0300

    feat: added function for searching fields

--- db-gen-api/src/Types/Database.hs
+++ db-gen-api/src/Types/Database.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Types.Database (DatabaseData (..)) where
+module Types.Database (DatabaseData (..), lookupField) where
 
 import           Data.Aeson
+import           Data.Text    (Text)
+import           Types.Column
 import           Types.Table
 
 newtype DatabaseData = DatabaseData [TableData] deriving Show
@@ -9,3 +11,14 @@ newtype DatabaseData = DatabaseData [TableData] deriving Show
 instance FromJSON DatabaseData where
   parseJSON = withObject "DatabaseData" $ \v -> DatabaseData
     <$> v .: "tables"
+
+type TableName = Text
+type FieldName = Text
+
+lookupField :: TableName -> FieldName -> DatabaseData -> Maybe ColumnData
+lookupField tName fName (DatabaseData tables) = do
+  case filter ((==tName) . getTableName ) tables of
+    [] -> Nothing
+    ((TableData _ cols):_) -> case filter ((==fName) . getColumnName) cols of
+      []    -> Nothing
+      (c:_) -> Just c
