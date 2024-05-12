26ff9f7ab0ec5ca8f8f8454aa5a12054ed2d21b9
mrtstg <russiankurwacsgo@gmail.com>
2024-05-12 16:37:22 +0300

    feat: added reference render

--- db-gen-api/src/Render.hs
+++ db-gen-api/src/Render.hs
@@ -1,8 +1,10 @@
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 module Render (renderColumn, renderTableCreate, renderDatabaseCreate) where
 
 import           Data.List      (intercalate)
 import           Data.Text      (unpack)
+import qualified Data.Text      as T
 import           Types.Column
 import           Types.Database
 import           Types.Table
@@ -17,6 +19,11 @@ renderColumn (ColumnData { .. }) =
   <> (if getColumnNullable then "NULL " else "")
   <> (if getColumnUnique then "UNIQUE " else "")
   <> (if getColumnPrimary then "PRIMARY KEY " else "")
+  <> reference where
+    reference :: String
+    reference = case getColumnReferenceOn of
+      Nothing  -> ""
+      (Just v) -> "REFERENCES " <> unpack (T.replace "." "(" v) <> ")"
 
 renderTableCreate :: TableData -> String
 renderTableCreate (TableData { .. }) = "CREATE TABLE " <> ['"'] <> unpack getTableName <> ['"'] <> "(" <> intercalate "," (map renderColumn getTableColumns) <> ");"
