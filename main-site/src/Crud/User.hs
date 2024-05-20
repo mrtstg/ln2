02233600fcf16a9b6e233045fa8ae7c1858c9260
mrtstg <russiankurwacsgo@gmail.com>
2024-05-20 19:03:09 +0300

    refactor+feat: added functions for getting member courses and admin courses from user roles

--- main-site/src/Crud/User.hs
+++ main-site/src/Crud/User.hs
@@ -1,13 +1,26 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Crud.User
   ( retrieveCourseUsers
+  , getUserMemberCourses
+  , getUserAdminCourses
   ) where
 
 import           Api.User
 import qualified Data.Map         as M
+import           Data.Models.Role
 import           Data.Models.User
+import           Data.Text        (unpack)
+import qualified Data.Text        as T
 import           Database.Persist
 import           Foundation
+
+getUserAdminCourses :: [RoleDetails] -> [CourseId]
+getUserAdminCourses roles = map (\(RoleDetails name _) -> (CourseKey . unpack . T.drop 7) name) $
+  filter (\(RoleDetails name _ ) -> T.take 8 name == "admins-") roles
+
+getUserMemberCourses :: [RoleDetails] -> [CourseId]
+getUserMemberCourses roles = map (\(RoleDetails name _) -> (CourseKey . unpack . T.drop 8) name) $
+    filter (\(RoleDetails name _) -> T.take 8 name == "members-") roles
 
 retrieveCourseUsers :: [Entity Course] -> IO (M.Map Int (UserGetResult UserDetails))
 retrieveCourseUsers = helper M.empty where
