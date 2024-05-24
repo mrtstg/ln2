5618bea23ed6c0ca09b87deea2addf24432934d6
mrtstg <russiankurwacsgo@gmail.com>
2024-05-24 21:21:13 +0300

    feat: added function for prettyfying task status text

--- main-site/src/Utils.hs
+++ main-site/src/Utils.hs
@@ -2,11 +2,21 @@
 module Utils
   ( constructPostgreStringFromEnv
   , unsafeRandomString
+  , taskStatusToText
   ) where
 
+import           Data.Text          (Text)
 import           System.Environment
 import           System.IO.Unsafe
 import           System.Random
+
+taskStatusToText :: String -> Text
+taskStatusToText "queued"     = "В очереди на выполнение"
+taskStatusToText "taken"      = "Взята на выполнение"
+taskStatusToText "error"      = "Завершена с ошибкой"
+taskStatusToText "processing" = "В обработке"
+taskStatusToText "finished"   = "Завершена"
+taskStatusToText _            = "Неизвестный статус"
 
 unsafeRandomString :: Int -> String
 unsafeRandomString n = take n $ randomRs ('a', 'z') $ unsafePerformIO newStdGen
