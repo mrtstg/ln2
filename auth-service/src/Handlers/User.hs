90e308eaa0a398376edd1464afc4bf181324fb4f
mrtstg <russiankurwacsgo@gmail.com>
2024-05-29 09:12:49 +0300

    feat: added check for user creating

--- auth-service/src/Handlers/User.hs
+++ auth-service/src/Handlers/User.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE RecordWildCards   #-}
 module Handlers.User (postUserRouteR) where
 
+import           Control.Monad      (when)
 import           Data.Aeson
 import           Data.Models.User
 import qualified Data.Text          as T
@@ -17,9 +18,14 @@ postUserRouteR = do
   UserCreate { .. } <- requireCheckJsonBody
   userExists <- runDB $ exists [UserLogin ==. getUserCreateLogin]
   if userExists then do
-    let err = T.pack $ "User with login " ++ T.unpack getUserCreateLogin ++ " already exists!"
-    sendStatusJSON status400 $ object ["error" .= String err]
+    sendStatusJSON status400 $ object ["error" .= String "Login is taken"]
     else do
+      when (T.null getUserCreateLogin) $ sendStatusJSON status400 $ object ["error" .= String "Login is empty"]
+      when (T.length getUserCreateLogin > 30) $ sendStatusJSON status400 $ object ["error" .= String "Login is too long"]
+      when (T.null getUserCreateName) $ sendStatusJSON status400 $ object ["error" .= String "User name is empty"]
+      when (T.length getUserCreateName > 50) $ sendStatusJSON status400 $ object ["error" .= String "User name is too long"]
+      when (T.null getUserCreatePassword) $ sendStatusJSON status400 $ object ["error" .= String "Password is empty"]
+      when (T.length getUserCreatePassword > 30) $ sendStatusJSON status400 $ object ["error" .= String "Password is too long"]
       let pwdHashText = sha256Text getUserCreatePassword
       (UserKey uid) <- runDB $ do
         insert $ User getUserCreateLogin getUserCreateName pwdHashText
