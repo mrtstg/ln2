298ba76aeba6e31362550c0eeb71dc8caad61198
mrtstg <russiankurwacsgo@gmail.com>
2024-05-29 09:12:29 +0300

    feat: added user create model

--- main-site/src/Data/Models/User.hs
+++ main-site/src/Data/Models/User.hs
@@ -1,12 +1,34 @@
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
-module Data.Models.User (UserDetails(..)) where
+module Data.Models.User
+  ( UserDetails(..)
+  , UserCreate(..)
+  ) where
 
 import           Data.Aeson
 import           Data.Models.Role
 import           Data.Text        (Text)
 import           GHC.Generics
+
+data UserCreate = UserCreate
+  { getUserCreateLogin    :: !Text
+  , getUserCreateName     :: !Text
+  , getUserCreatePassword :: !Text
+  } deriving (Show)
+
+instance FromJSON UserCreate where
+  parseJSON = withObject "UserCreate" $ \v -> UserCreate
+    <$> v .: "login"
+    <*> v .: "name"
+    <*> v .: "password"
+
+instance ToJSON UserCreate where
+  toJSON (UserCreate { .. }) = object
+    [ "login" .= getUserCreateLogin
+    , "name" .= getUserCreateName
+    , "password" .= getUserCreatePassword
+    ]
 
 data UserDetails = UserDetails
   { getUserDetailsId    :: !Int
