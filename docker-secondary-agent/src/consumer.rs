7698a22e0360839c2216b602688de091ab038eea
mrtstg <russiankurwacsgo@gmail.com>
2024-07-02 21:05:17 +0300

    refactor: adapted consumer for lapin

--- docker-secondary-agent/src/consumer.rs
+++ docker-secondary-agent/src/consumer.rs
@@ -3,12 +3,11 @@ use crate::structs::app_env::{self, AppEnvironment};
 use crate::structs::check_result::StandCheckResult;
 use crate::structs::queue_response::QueueTaskResponse;
 use crate::structs::queue_task::QueueTask;
-use amqprs::channel::{self, BasicPublishArguments, Channel};
-use amqprs::BasicProperties;
-use amqprs::{channel::BasicAckArguments, consumer::AsyncConsumer};
 use async_std::future::timeout;
-use async_trait::async_trait;
 use docker_api::docker::Docker;
+use lapin::message::Delivery;
+use lapin::options::{BasicAckOptions, BasicPublishOptions};
+use lapin::{BasicProperties, Channel, Connection};
 use log::*;
 use serde_json::from_str;
 use std::time::Duration;
@@ -16,26 +15,19 @@ use std::time::Duration;
 pub struct RabbitConsumer {
     pub docker_client: Docker,
     pub app_env: AppEnvironment,
-    pub publish_args: BasicPublishArguments,
 }
 
 impl RabbitConsumer {
-    pub fn new(
-        app_env: AppEnvironment,
-        publish_args: BasicPublishArguments,
-        docker_client: Docker,
-    ) -> Self {
+    pub fn new(app_env: AppEnvironment, docker_client: Docker) -> Self {
         RabbitConsumer {
             app_env,
-            publish_args,
             docker_client,
         }
     }
 }
 
 async fn update_task_status(
-    channel: &Channel,
-    publish_args: BasicPublishArguments,
+    connection: &Connection,
     uuid: String,
     status: String,
     result: Option<StandCheckResult>,
@@ -48,36 +40,42 @@ async fn update_task_status(
     info!("Task {}: state {}", uuid, status);
     match serde_json::to_string(&data) {
         Ok(string) => {
-            let content = string.as_bytes().to_vec();
+            let channel_res = connection.create_channel().await;
+            if let Err(e) = channel_res {
+                error!("Failed to open channel: {:?}", e);
+                return Err(e.to_string());
+            }
+            let channel = channel_res.unwrap();
+            let content = string.as_bytes();
             match channel
-                .basic_publish(BasicProperties::default(), content, publish_args)
+                .basic_publish(
+                    "resultsExchange",
+                    "",
+                    BasicPublishOptions::default(),
+                    content,
+                    BasicProperties::default(),
+                )
                 .await
             {
                 Ok(_) => return Ok(()),
-                Err(e) => return Err(e.to_string()),
+                Err(e) => {
+                    error!("Task update error: {:?}", e);
+                    return Err(e.to_string());
+                }
             }
         }
         Err(e) => return Err(e.to_string()),
     }
 }
 
-#[async_trait]
-impl AsyncConsumer for RabbitConsumer {
-    async fn consume<'life0, 'life1>(
-        &'life0 mut self,
-        channel: &'life1 amqprs::channel::Channel,
-        deliver: amqprs::Deliver,
-        _basic_properties: amqprs::BasicProperties,
-        content: Vec<u8>,
-    ) where
-        'life0: 'async_trait,
-        'life1: 'async_trait,
-        Self: 'async_trait,
-    {
-        let data = String::from_utf8(content);
-        let _ = channel
-            .basic_ack(BasicAckArguments::new(deliver.delivery_tag(), false))
-            .await;
+impl RabbitConsumer {
+    pub async fn consume(&self, delivery: Delivery, conn: &Connection) {
+        let data = String::from_utf8(delivery.data.clone());
+        let chan_res = conn.create_channel().await;
+        if let Err(ref e) = chan_res {
+            error!("Failed to open channel: {:?}", e);
+        }
+        let _ = delivery.ack(BasicAckOptions::default()).await;
         match data {
             Err(e) => error!("Failed to serialize message into string: {:?}", e),
             Ok(message) => {
@@ -85,8 +83,7 @@ impl AsyncConsumer for RabbitConsumer {
                 match parse_res {
                     Ok(queue_task) => {
                         let _ = update_task_status(
-                            channel,
-                            self.publish_args.clone(),
+                            conn,
                             queue_task.uuid.clone(),
                             "taken".to_string(),
                             None,
@@ -102,8 +99,7 @@ impl AsyncConsumer for RabbitConsumer {
                         {
                             Ok((network, containers)) => {
                                 let _ = update_task_status(
-                                    channel,
-                                    self.publish_args.clone(),
+                                    conn,
                                     queue_task.uuid.clone(),
                                     "processing".to_string(),
                                     None,
@@ -123,8 +119,7 @@ impl AsyncConsumer for RabbitConsumer {
                                 {
                                     Ok((_, res)) => {
                                         let _ = update_task_status(
-                                            channel,
-                                            self.publish_args.clone(),
+                                            conn,
                                             queue_task.uuid.clone(),
                                             "finished".to_string(),
                                             Some(res),
@@ -134,8 +129,7 @@ impl AsyncConsumer for RabbitConsumer {
                                     Err(_) => {
                                         error!("Timeout error as task {}", queue_task.uuid.clone());
                                         let _ = update_task_status(
-                                            channel,
-                                            self.publish_args.clone(),
+                                            conn,
                                             queue_task.uuid.clone(),
                                             "timeout".to_string(),
                                             None,
@@ -146,10 +140,10 @@ impl AsyncConsumer for RabbitConsumer {
                                 destroy_stand(network, containers.values().map(|v| v).collect())
                                     .await;
                             }
-                            Err(_) => {
+                            Err(e) => {
+                                error!("Failed to finish task: {}", e);
                                 let _ = update_task_status(
-                                    channel,
-                                    self.publish_args.clone(),
+                                    conn,
                                     queue_task.uuid,
                                     "error".to_string(),
                                     None,
