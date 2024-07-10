1ae7626a9d257cad0a17085fd7cc0e97b05363e7
mrtstg <russiankurwacsgo@gmail.com>
2024-07-10 22:43:22 +0300

    feat: added support to new statuses

--- main-site/src/Utils.hs
+++ main-site/src/Utils.hs
@@ -16,6 +16,9 @@ taskStatusToText "taken"      = "Взята на выполнение"
 taskStatusToText "error"      = "Завершена с ошибкой"
 taskStatusToText "processing" = "В обработке"
 taskStatusToText "finished"   = "Завершена"
+taskStatusToText "accepted"   = "Зачтена"
+taskStatusToText "cancelled"  = "Проверка прервана"
+taskStatusToText "timeout"    = "Тайм-аут проверки"
 taskStatusToText _            = "Неизвестный статус"
 
 unsafeRandomString :: Int -> String
