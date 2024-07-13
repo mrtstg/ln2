44acc3206b1e94ec3c8644dbf477c76a75bfbc1e
mrtstg <russiankurwacsgo@gmail.com>
2024-07-13 23:13:38 +0300

    feat+refactor: simplified component code, adapted part of fields

--- frontend/components/CheckStage.svelte
+++ frontend/components/CheckStage.svelte
@@ -6,6 +6,7 @@
   import DangerMessage from "./DangerMessage.svelte"
   import SelectField from "./SelectField.svelte"
   import DBConstructor from "./DBConstructor.svelte"
+  import CheckStageContainer from "./CheckStageContainer.svelte"
 
   export let data: CheckStage
   export let containers: string[]
@@ -22,6 +23,10 @@
     }
   }
 
+  const updateCallbackWrapper = async () => {
+    await updateCallback(data)
+  }
+
   const updateCheckStage = async () => {
     data.type = stageTypeList[selectedActionIndex]
     data.data = defaultCheckStageData(data.type)
@@ -29,25 +34,33 @@
   }
 </script>
 
-<div class="box my-3">
-  <div class="field">
-    <label class="label"> Тип действия </label>
-    <div class="control">
-      <div class="select">
-        <select bind:value={selectedActionIndex} on:change={async (event) => { await updateCheckStage() }}>
-          {#each stageTypeList as stageType, stageIndex}
-            <option value={stageIndex}> {checkStageName(stageType)} </option>
-          {/each}
-        </select>
+<div class="my-3">
+  <div class="flex flex-row justify-between">
+    <div class="field">
+      <label class="label"> Тип действия </label>
+      <div class="control">
+        <div class="select">
+          <select bind:value={selectedActionIndex} on:change={async (event) => { await updateCheckStage() }}>
+            {#each stageTypeList as stageType, stageIndex}
+              <option value={stageIndex}> {checkStageName(stageType)} </option>
+            {/each}
+          </select>
+        </div>
       </div>
     </div>
+    <button on:click={deleteCallback} class="text-white">
+      <div class="icon medium bg-red-700 font-bold rounded-sm text-xl">
+        -
+      </div>
+    </button>
   </div>
+  
   {#if selectedType == StageType.CopyFile }
-    <SelectField title="Контейнер выполнения" items={containers} selectCallback={async (v) => { data.data.container = v; await updateCallback(data) }}/>
+    <SelectField title="Контейнер выполнения" items={containers} selectCallback={async (v) => { data.data.target = v; await updateCallbackWrapper() }}/>
     <div class="field">
       <label class="label"> Путь файла </label>
       <div class="control">
-        <input class="input" type="text" placeholder="Путь файла, например /a.sql" bind:value={data.data.filePath} on:change={async () => await updateCallback(data) }>
+        <input class="input" type="text" placeholder="Путь файла, например /a.sql" bind:value={data.data.path} on:change={updateCallbackWrapper}>
       </div>
     </div>
     <div class="field">
@@ -57,44 +70,44 @@
           "&": {
           "font-size": "1rem"
           }
-        }} bind:value={data.data.fileContent} on:change={async () => await updateCallback(data) }/>
+        }} bind:value={data.data.content} on:change={updateCallbackWrapper }/>
       </div>
     </div>
   {:else if selectedType == StageType.ExecuteCommand }
-    <SelectField title="Контейнер выполнения" items={containers} selectCallback={async (v) => { data.data.container = v; await updateCallback(data) }}/>
+    <SelectField title="Контейнер выполнения" items={containers} selectCallback={async (v) => { data.data.target = v; await updateCallback(data) }}/>
     <div class="field">
       <label class="label"> Команда для выполнения </label>
       <div class="control">
-        <input class="input" type="text" placeholder="Команда, например psql -f /a.sql" bind:value={data.data.command} on:change={async () => await updateCallback(data) }>
+        <input class="input" type="text" placeholder="Команда, например psql -f /a.sql" bind:value={data.data.command} on:change={updateCallbackWrapper }>
       </div>
     </div>
     <label class="checkbox">
-      <input type="checkbox" bind:checked={data.data.formatOutput} on:change={async () => await updateCallback(data)}/>
+      <input type="checkbox" bind:checked={data.data.formatOutput} on:change={updateCallbackWrapper}/>
       Форматировать вывод
     </label>
     <label class="checkbox">
-      <input type="checkbox" bind:checked={data.data.reportError} on:change={async () => await updateCallback(data)}/>
+      <input type="checkbox" bind:checked={data.data.reportError} on:change={updateCallbackWrapper}/>
       Сообщать пользователю об ошибках в процессе выполнения
     </label>
     <div class="field">
       <label class="label"> Записать в переменную (<i>опционально</i>)</label>
       <div class="control">
-        <input class="input" type="text" placeholder="Название переменной, например result" maxlength="25" bind:value={data.data.recordInto} on:change={async () => await updateCallback(data)}>
+        <input class="input" type="text" placeholder="Название переменной, например result" maxlength="25" bind:value={data.data.recordInto} on:change={updateCallbackWrapper}>
       </div>
     </div>
   {:else if selectedType == StageType.CopyAnswer }
-    <SelectField title="Скопировать на" items={containers} selectCallback={async (v) => { data.data.container = v; await updateCallback(data) }}/>
+    <SelectField title="Скопировать на" items={containers} selectCallback={async (v) => { data.data.target = v; await updateCallback(data) }}/>
     <div class="field">
       <label class="label"> Путь файла </label>
       <div class="control">
-        <input class="input" type="text" placeholder="Путь для сохранения, например a.sql" bind:value={data.data.filePath} on:change={async () => await updateCallback(data)}>
+        <input class="input" type="text" placeholder="Путь для сохранения, например a.sql" bind:value={data.data.filePath} on:change={updateCallbackWrapper}>
       </div>
     </div>
   {:else if selectedType == StageType.DeclareVariable}
     <div class="field">
       <label class="label"> Название </label>
       <div class="control">
-        <input class="input" type="text" placeholder="Название переменной, например result" maxlength="25" bind:value={data.data.variableName} on:change={async () => await updateCallback(data)}>
+        <input class="input" type="text" placeholder="Название переменной, например result" maxlength="25" bind:value={data.data.variableName} on:change={updateCallbackWrapper}>
       </div>
     </div>
     <div class="field">
@@ -104,36 +117,36 @@
           "&": {
           "font-size": "1rem"
           }
-        }} bind:value={data.data.variableValue} on:change={async () => await updateCallback(data) }/>
+        }} bind:value={data.data.variableValue} on:change={updateCallbackWrapper }/>
       </div>
     </div>
-  {:else if selectedType == StageType.CompareResults}
+  {:else if selectedType == StageType.CompareVariables}
     <div class="field">
       <label class="label"> Первая переменная </label>
       <div class="control">
-        <input class="input" type="text" placeholder="Название переменной" maxlength="25" bind:value={data.data.first} on:change={async () => await updateCallback(data)}>
+        <input class="input" type="text" placeholder="Название переменной" maxlength="25" bind:value={data.data.first} on:change={updateCallbackWrapper}>
       </div>
     </div>
     <div class="field">
       <label class="label"> Вторая переменная </label>
       <div class="control">
-        <input class="input" type="text" placeholder="Название переменной" maxlength="25" bind:value={data.data.second} on:change={async () => await updateCallback(data)}>
+        <input class="input" type="text" placeholder="Название переменной" maxlength="25" bind:value={data.data.second} on:change={updateCallbackWrapper}>
       </div>
     </div>
-    <div class="field">
-      <label class="label"> Количество зачтенных баллов </label>
-      <div class="control">
-        <input class="input" type="number" placeholder="Количество баллов" bind:value={data.data.score} on:change={async () => await updateCallback(data)}>
-      </div>
-    </div>
-    <div class="field">
-      <label class="label"> Текст сообщения при несоответствии данных (<i> опционально</i>) </label>
-      <div class="control">
-        <input class="input" type="text" placeholder="Сообщение" bind:value={data.data.failureMessage} on:change={async () => await updateCallback(data)}> 
-      </div>
-    </div>
+    <CheckStageContainer
+      stages={data.data.positiveActions}
+      containers={containers}
+      title="Если переменные равны:"
+      updateCallback={async (newStages) => {data.data.positiveActions = newStages; await updateCallbackWrapper()}}
+    />
+    <CheckStageContainer
+      stages={data.data.negativeActions}
+      containers={containers}
+      title="Если переменные не равны:"
+      updateCallback={async (newStages) => {data.data.negativeActions = newStages}}
+    />
   {:else if selectedType == StageType.PSQLQuery}
-    <SelectField title="Контейнер выполнения" items={containers} selectCallback={async (v) => { data.data.container = v; await updateCallback(data) }}/>
+    <SelectField title="Контейнер выполнения" items={containers} selectCallback={async (v) => { data.data.target = v; await updateCallback(data) }}/>
     <div class="field">
       <label class="label"> SQL-запрос </label>
       <div class="control">
@@ -141,25 +154,25 @@
           "&": {
           "font-size": "1rem"
           }
-        }} bind:value={data.data.query} on:change={async () => await updateCallback(data) }/>
+        }} bind:value={data.data.query} on:change={updateCallbackWrapper }/>
       </div>
       </div>
     <div class="field">
       <label class="label"> Записать в переменную (<i>опционально</i>)</label>
       <div class="control">
-        <input class="input" type="text" placeholder="Название переменной, например result" maxlength="25" bind:value={data.data.recordInto} on:change={async () => await updateCallback(data)}>
+        <input class="input" type="text" placeholder="Название переменной, например result" maxlength="25" bind:value={data.data.recordInto} on:change={updateCallbackWrapper}>
       </div>
     </div>
   {:else if selectedType == StageType.PSQLAnswerQuery}
-    <SelectField title="Контейнер выполнения" items={containers} selectCallback={async (v) => { data.data.container = v; await updateCallback(data) }}/>
+    <SelectField title="Контейнер выполнения" items={containers} selectCallback={async (v) => { data.data.target = v; await updateCallback(data) }}/>
     <div class="field">
       <label class="label"> Записать в переменную (<i>опционально</i>)</label>
       <div class="control">
-        <input class="input" type="text" placeholder="Название переменной, например result" maxlength="25" bind:value={data.data.recordInto} on:change={async () => await updateCallback(data)}>
+        <input class="input" type="text" placeholder="Название переменной, например result" maxlength="25" bind:value={data.data.recordInto} on:change={updateCallbackWrapper}>
       </div>
     </div>
   {:else if selectedType == StageType.PSQLExists}
-    <SelectField title="Контейнер выполнения" items={containers} selectCallback={async (v) => { data.data.container = v; await updateCallback(data) }}/>
+    <SelectField title="Контейнер выполнения" items={containers} selectCallback={async (v) => { data.data.target = v; await updateCallback(data) }}/>
     <div class="field">
       <label class="label"> SQL-запрос </label>
       <div class="control">
@@ -167,74 +180,73 @@
           "&": {
           "font-size": "1rem"
           }
-        }} bind:value={data.data.query} on:change={async () => await updateCallback(data) }/>
+        }} bind:value={data.data.query} on:change={updateCallbackWrapper }/>
       </div>
       <p class="help"> Запрос должен выполнять поиск определенных данных в единичном количестве. Запрос будет автоматически подставлен. Не ставьте точку с запятой в конце. </p>
     </div>
     <div class="field">
       <label class="label"> Количество зачтенных баллов, если данные в базе есть </label>
       <div class="control">
-        <input class="input" type="number" placeholder="Количество баллов" bind:value={data.data.score} on:change={async () => await updateCallback(data)}>
+        <input class="input" type="number" placeholder="Количество баллов" bind:value={data.data.score} on:change={updateCallbackWrapper}>
       </div>
     </div>
   {:else if selectedType == StageType.PSQLGenerateDatabase}
-    <SelectField title="Контейнер выполнения" items={containers} selectCallback={async (v) => { data.data.container = v; await updateCallback(data) }}/>
+    <SelectField title="Контейнер выполнения" items={containers} selectCallback={async (v) => { data.data.target = v; await updateCallback(data) }}/>
     <DBConstructor dbData={data.data.database} changeCallback={(t) => data.data.database.tables = t}/>
     <br>
   {:else if selectedType == StageType.PSQLTableExists}
-    <SelectField title="Контейнер выполнения" items={containers} selectCallback={async (v) => { data.data.container = v; await updateCallback(data) }}/>
+    <SelectField title="Контейнер выполнения" items={containers} selectCallback={async (v) => { data.data.target = v; await updateCallback(data) }}/>
     <div class="field">
       <label class="label"> Название таблицы </label>
       <div class="control">
-        <input class="input" type="text" placeholder="Название таблицы, без кавычек" bind:value={data.data.tableName} on:change={async () => await updateCallback(data)}>
+        <input class="input" type="text" placeholder="Название таблицы, без кавычек" bind:value={data.data.tableName} on:change={updateCallbackWrapper}>
       </div>
     </div>
     <div class="field">
       <label class="label"> Количество зачтенных баллов, если таблица есть </label>
       <div class="control">
-        <input class="input" type="number" placeholder="Количество баллов" bind:value={data.data.score} on:change={async () => await updateCallback(data)}>
+        <input class="input" type="number" placeholder="Количество баллов" bind:value={data.data.score} on:change={updateCallbackWrapper}>
       </div>
     </div>
     <div class="field">
       <label class="checkbox">
-        <input type="checkbox" bind:checked={data.data.reportError} on:change={async () => await updateCallback(data)}/>
+        <input type="checkbox" bind:checked={data.data.reportError} on:change={updateCallbackWrapper}/>
         Вывести ошибку пользователю, если таблица не найдена
       </label>
     </div>
   {:else if selectedType == StageType.PSQLColumnTypeCheck}
-    <SelectField title="Контейнер выполнения" items={containers} selectCallback={async (v) => { data.data.container = v; await updateCallback(data) }}/>
+    <SelectField title="Контейнер выполнения" items={containers} selectCallback={async (v) => { data.data.target = v; await updateCallback(data) }}/>
     <div class="field">
       <label class="label"> Название таблицы </label>
       <div class="control">
-        <input class="input" type="text" placeholder="Название таблицы, без кавычек" bind:value={data.data.tableName} on:change={async () => await updateCallback(data)}>
+        <input class="input" type="text" placeholder="Название таблицы, без кавычек" bind:value={data.data.tableName} on:change={updateCallbackWrapper}>
       </div>
     </div>
     <div class="field">
       <label class="label"> Название столбца </label>
       <div class="control">
-        <input class="input" type="text" placeholder="Название столбца" bind:value={data.data.columnName} on:change={async () => await updateCallback(data)}>
+        <input class="input" type="text" placeholder="Название столбца" bind:value={data.data.columnName} on:change={updateCallbackWrapper}>
       </div>
     </div>
     <div class="field">
       <label class="label"> Ожидаемый тип </label>
       <div class="control">
-        <input class="input" type="text" placeholder="Тип данных" bind:value={data.data.awaitedType} on:change={async () => await updateCallback(data)}>
+        <input class="input" type="text" placeholder="Тип данных" bind:value={data.data.awaitedType} on:change={updateCallbackWrapper}>
       </div>
     </div>
     <div class="field">
       <label class="label"> Количество зачтенных баллов, если тип совпал </label>
       <div class="control">
-        <input class="input" type="number" placeholder="Количество баллов" bind:value={data.data.score} on:change={async () => await updateCallback(data)}>
+        <input class="input" type="number" placeholder="Количество баллов" bind:value={data.data.score} on:change={updateCallbackWrapper}>
       </div>
     </div>
     <div class="field">
       <label class="checkbox">
-        <input type="checkbox" bind:checked={data.data.reportError} on:change={async () => await updateCallback(data)}/>
+        <input type="checkbox" bind:checked={data.data.reportError} on:change={updateCallbackWrapper}/>
         Вывести ошибку пользователю, если тип не совпадает
       </label>
     </div>
   {:else}
     <DangerMessage title="Ошибка!" description="Неизвестный тип"/>
   {/if}
-  <button class="button is-danger is-fullwidth" on:click={deleteCallback}> Удалить </button>
 </div>
