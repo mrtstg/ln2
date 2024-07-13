f9f838c43abc053dd3c7ffdc21a88974fbf0261b
mrtstg <russiankurwacsgo@gmail.com>
2024-07-13 21:43:02 +0300

    fix: simplified mask for faster build

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,svelte,js,ts}"],
+  content: ["./**/src/*.{html,svelte,js,ts}"],
   theme: {
     extend: {},
   },
