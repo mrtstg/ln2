e47ad8b4fa27bc436ef151c1f1a2c297bb1c222a
mrtstg <russiankurwacsgo@gmail.com>
2024-07-23 22:43:34 +0300

    feat: added svelte-codemirror library, added component for default field

---
+++ frontend/components/CodemirrorField.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import CodeMirror, { basicSetup } from "codemirror-svelte"
+  import { EditorView } from "@codemirror/view";
+
+  export let doc: string;
+  export let onChange: (doc: string) => Promise<void>;
+
+  $: doc && docChanged()
+
+  const docChanged = () => {
+    onChange(doc)
+  }
+
+  let extensions = [
+    basicSetup,
+    EditorView.theme({
+      "&": {
+        "font-size": "1rem"
+      }
+    })
+  ]
+</script>
+
+<CodeMirror {doc} {extensions}/>
