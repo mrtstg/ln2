8c6d6bbc50f2fd63206a71e792a79e7f29942955
mrtstg <russiankurwacsgo@gmail.com>
2024-07-23 22:09:07 +0300

    fix: removed using of currently not existing CSS file

--- main-site/src/Handlers/CourseTask.hs
+++ main-site/src/Handlers/CourseTask.hs
@@ -120,9 +120,6 @@ getCourseTaskR ctId = do
             parseRes <- liftIO $ convertMarkdown' courseTaskContent
             defaultLayout $ do
               setTitle $ toHtml ("Задача: " <> T.unpack courseTaskName)
-              toWidgetHead [hamlet|
-<link rel=stylesheet href=/static/css/courseTaskForm.css>
-|]
               [whamlet|
 <div .container.pt-2.py-3>
   <nav .breadcrumb>
