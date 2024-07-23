56685cc3ab06159960cfec2762fa0848bec63b9d
mrtstg <russiankurwacsgo@gmail.com>
2024-07-23 23:13:16 +0300

    fix: removed using of currently not existing CSS file

--- main-site/src/Handlers/CoursePage.hs
+++ main-site/src/Handlers/CoursePage.hs
@@ -48,9 +48,6 @@ getAdminCourseR cId = do
   userD <- requireAuth
   _ <- validateCourseId cId userD isUserCourseAdmin AdminCoursesR
   defaultLayout $ do
-    toWidgetHead [hamlet|
-<link rel=stylesheet href=/static/css/courseTaskCreateForm.css>
-|]
     [whamlet|
       <div #app>
       <script src="/static/js/adminCourseForm.js">|]
