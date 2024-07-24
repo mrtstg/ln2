b5c9d75e3967e465e2ded46c129bb9e4edbfc34b
mrtstg <russiankurwacsgo@gmail.com>
2024-07-24 19:07:52 +0300

    feat: fixed codemirror doc reactivity

--- frontend/components/CodemirrorField.svelte
+++ frontend/components/CodemirrorField.svelte
@@ -1,21 +1,25 @@
 <script lang="ts">
   import CodeMirror, { basicSetup } from "codemirror-svelte"
-  import { EditorView } from "@codemirror/view";
+  import { EditorView, ViewPlugin } from "@codemirror/view";
 
   export let doc: string;
   export let onChange: (doc: string) => Promise<void>;
-
-  $: doc && docChanged()
-
-  const docChanged = () => {
-    onChange(doc)
-  }
 
   let extensions = [
     basicSetup,
     EditorView.theme({
       "&": {
         "font-size": "1rem"
+      }
+    }),
+    ViewPlugin.fromClass(class {
+      constructor(view) {}
+
+      update(update) {
+        if (update.docChanged) {
+          doc = update.state.doc.text.join('\n')
+          onChange(doc).then()
+        }
       }
     })
   ]
