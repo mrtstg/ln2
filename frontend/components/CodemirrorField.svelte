ebe6fcc572acf1a38616c266c302f8d66bee5485
mrtstg <russiankurwacsgo@gmail.com>
2024-07-26 16:04:57 +0300

    feat: added export function for cleaning codemirror field

--- frontend/components/CodemirrorField.svelte
+++ frontend/components/CodemirrorField.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
   import CodeMirror, { basicSetup } from "codemirror-svelte"
-  import { EditorView, ViewPlugin, keymap } from "@codemirror/view";
+  import { EditorViewConfig, EditorView, ViewPlugin, keymap } from "@codemirror/view";
   import { EditorState } from "@codemirror/state";
   import { indentWithTab } from "@codemirror/commands"
 
   export let readonly: boolean = false;
   export let doc: string;
   export let onChange: (doc: string) => Promise<void> = async (arg0) => {};
+
+  let codemirrorView: EditorView | undefined = undefined
+
+  export const cleanField = () => {
+    if (codemirrorView != undefined) {
+      codemirrorView.setState(EditorState.create({doc: '', extensions: extensions}))
+    }
+  }
 
   let extensions = [
     basicSetup,
@@ -17,7 +25,9 @@
       }
     }),
     ViewPlugin.fromClass(class {
-      constructor(view) {}
+      constructor(view) {
+        codemirrorView = view
+      }
 
       update(update) {
         if (update.docChanged) {
