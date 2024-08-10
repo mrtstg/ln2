c8e95de3cfc6512af02c0cda55fab87e57aaa406
mrtstg <russiankurwacsgo@gmail.com>
2024-08-10 21:26:46 +0300

    feat: added field for service simple SDN zone

--- proxmox-deploy-api/src/Data/Models/ProxmoxConfiguration.hs
+++ proxmox-deploy-api/src/Data/Models/ProxmoxConfiguration.hs
@@ -14,6 +14,7 @@ data ProxmoxConfiguration = ProxmoxConfiguration
   , proxmoxFSAgentToken  :: !Text
   , proxmoxVerifySSL     :: !Bool
   , proxmoxNodeName      :: !Text
+  , proxmoxSDNZone       :: !Text
   } deriving (Show)
 
 getProxmoxConfigurationFromEnv :: IO (Maybe ProxmoxConfiguration)
@@ -29,12 +30,14 @@ getProxmoxConfigurationFromEnv = let
   proxmox_fs_token'<- lookupEnv "PROXMOX_AGENT_ACCESS_TOKEN"
   proxmox_verify_ssl' <- lookupEnv "PROXMOX_VERIFY_SSL"
   proxmox_node_name' <- lookupEnv "PROXMOX_NODE_NAME"
+  proxmox_sdn_zone' <- lookupEnv "PROXMOX_DEPLOY_SDN_ZONE"
   return $ do
     proxmox_url <- proxmox_url'
     proxmox_token <- proxmox_token'
     proxmox_token_id <- proxmox_token_id'
     proxmox_fs_token <- proxmox_fs_token'
     proxmox_node_name <- proxmox_node_name'
+    proxmox_sdn_zone <- proxmox_sdn_zone'
     let proxmox_verify_ssl = boolWrapper proxmox_verify_ssl'
     return $ ProxmoxConfiguration
       { proxmoxBaseUrl = pack proxmox_url
@@ -43,4 +46,5 @@ getProxmoxConfigurationFromEnv = let
       , proxmoxFSAgentToken = pack proxmox_fs_token
       , proxmoxVerifySSL = proxmox_verify_ssl
       , proxmoxNodeName = pack proxmox_node_name
+      , proxmoxSDNZone = pack proxmox_sdn_zone
       }
