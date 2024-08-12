62fde4c199c4072e16b6e90e1b932b5c5e4f04b5
mrtstg <russiankurwacsgo@gmail.com>
2024-08-12 20:06:59 +0300

    feat: added SDN network configuration to proxmox configuration

--- proxmox-deploy-api/src/Data/Models/ProxmoxConfiguration.hs
+++ proxmox-deploy-api/src/Data/Models/ProxmoxConfiguration.hs
@@ -1,11 +1,55 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 module Data.Models.ProxmoxConfiguration
   ( ProxmoxConfiguration(..)
   , getProxmoxConfigurationFromEnv
+  , proxmoxNetworkConfigurationToPayload
+  , proxmoxOutNetwork
   ) where
 
+import           Data.Models.ProxmoxAPI.SDNSubnet (SDNSubnetCreate (..))
 import           Data.Text
 import           System.Environment
+
+proxmoxOutNetwork :: String
+proxmoxOutNetwork = "internet"
+
+data ProxmoxNetworkConfiguration = ProxmoxNetworkConfiguration
+  { proxmoxNetworkCIDR      :: !String
+  , proxmoxNetworkDHCPBegin :: !String
+  , proxmoxNetworkDHCPEnd   :: !String
+  , proxmoxNetworkGateway   :: !String
+  } deriving (Show)
+
+proxmoxNetworkConfigurationToPayload :: ProxmoxNetworkConfiguration -> SDNSubnetCreate
+proxmoxNetworkConfigurationToPayload ProxmoxNetworkConfiguration { .. } = SDNSubnetCreate
+  { getSDNSubnetCreateName = proxmoxNetworkCIDR
+  , getSDNSubnetCreateVnet = proxmoxOutNetwork
+  , getSDNSubnetCreateDhcpRange = Just ["start-address=" <> proxmoxNetworkDHCPBegin <> ",end-address=" <> proxmoxNetworkDHCPEnd]
+  , getSDNSubnetCreateDhcpDns = Nothing
+  , getSDNSubnetCreateGateway = Just proxmoxNetworkGateway
+  , getSDNSubnetCreateSnat = True
+  , getSDNSubnetCreateDnsPrefix = Nothing
+  }
+
+getProxmoxNetworkConfigurationFromEnv :: IO (Maybe ProxmoxNetworkConfiguration)
+getProxmoxNetworkConfigurationFromEnv = do
+  network_cidr' <- lookupEnv "PROXMOX_INTERNET_NETWORK_CIDR"
+  dhcp_begin' <- lookupEnv "PROXMOX_INTERNET_NETWORK_DHCP_BEGIN"
+  dhcp_end' <- lookupEnv "PROXMOX_INTERNET_NETWORK_DHCP_END"
+  gateway' <- lookupEnv "PROXMOX_INTERNET_NETWORK_GATEWAY"
+  return $ do
+    network_cidr <- network_cidr'
+    dhcp_begin <- dhcp_begin'
+    dhcp_end <- dhcp_end'
+    gateway <- gateway'
+    return $ ProxmoxNetworkConfiguration
+      { proxmoxNetworkCIDR = network_cidr
+      , proxmoxNetworkDHCPBegin = dhcp_begin
+      , proxmoxNetworkDHCPEnd = dhcp_end
+      , proxmoxNetworkGateway = gateway
+      }
 
 data ProxmoxConfiguration = ProxmoxConfiguration
   { proxmoxBaseUrl       :: !Text
@@ -15,6 +59,7 @@ data ProxmoxConfiguration = ProxmoxConfiguration
   , proxmoxVerifySSL     :: !Bool
   , proxmoxNodeName      :: !Text
   , proxmoxSDNZone       :: !Text
+  , proxmoxSDNNetwork    :: !ProxmoxNetworkConfiguration
   } deriving (Show)
 
 getProxmoxConfigurationFromEnv :: IO (Maybe ProxmoxConfiguration)
@@ -31,6 +76,7 @@ getProxmoxConfigurationFromEnv = let
   proxmox_verify_ssl' <- lookupEnv "PROXMOX_VERIFY_SSL"
   proxmox_node_name' <- lookupEnv "PROXMOX_NODE_NAME"
   proxmox_sdn_zone' <- lookupEnv "PROXMOX_DEPLOY_SDN_ZONE"
+  proxmox_sdn_network' <- getProxmoxNetworkConfigurationFromEnv
   return $ do
     proxmox_url <- proxmox_url'
     proxmox_token <- proxmox_token'
@@ -38,6 +84,7 @@ getProxmoxConfigurationFromEnv = let
     proxmox_fs_token <- proxmox_fs_token'
     proxmox_node_name <- proxmox_node_name'
     proxmox_sdn_zone <- proxmox_sdn_zone'
+    proxmox_sdn_network <- proxmox_sdn_network'
     let proxmox_verify_ssl = boolWrapper proxmox_verify_ssl'
     return $ ProxmoxConfiguration
       { proxmoxBaseUrl = pack proxmox_url
@@ -47,4 +94,5 @@ getProxmoxConfigurationFromEnv = let
       , proxmoxVerifySSL = proxmox_verify_ssl
       , proxmoxNodeName = pack proxmox_node_name
       , proxmoxSDNZone = pack proxmox_sdn_zone
+      , proxmoxSDNNetwork = proxmox_sdn_network
       }
