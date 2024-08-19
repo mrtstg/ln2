027b3da5d36d6b284ff816ef06d310f4c93c7f3a
mrtstg <russiankurwacsgo@gmail.com>
2024-08-19 13:41:00 +0300

    refactor: moved out function for getting int param

--- libs/ln2-common/src/Handlers/Params.hs
+++ libs/ln2-common/src/Handlers/Params.hs
@@ -3,6 +3,7 @@ module Handlers.Params
   ( getPageNumber
   , defaultPageSize
   , getBoolParameter
+  , getIntParam
   ) where
 
 import           Data.Text  (Text, unpack)
@@ -29,3 +30,14 @@ getPageNumber = do
       case pageV of
         Nothing   -> return 1
         (Just v') -> return $ max 1 v'
+
+getIntParam :: Text -> Int -> HandlerFor a Int
+getIntParam paramName defaultValue = do
+  queryValue <- lookupGetParam paramName
+  case queryValue of
+    Nothing -> return defaultValue
+    (Just v) -> do
+      let paramV = readMaybe $ unpack v :: Maybe Int
+      case paramV of
+        Nothing   -> return defaultValue
+        (Just v') -> return v'
--- proxmox-deploy-api/src/Handlers/Utils.hs
+++ proxmox-deploy-api/src/Handlers/Utils.hs
@@ -1,17 +1,4 @@
-module Handlers.Utils (getIntParam) where
+module Handlers.Utils () where
 
-import           Data.Text  (Text, unpack)
 import           Foundation
-import           Text.Read  (readMaybe)
 import           Yesod.Core
-
-getIntParam :: Text -> Int -> Handler Int
-getIntParam paramName defaultValue = do
-  queryValue <- lookupGetParam paramName
-  case queryValue of
-    Nothing -> return defaultValue
-    (Just v) -> do
-      let paramV = readMaybe $ unpack v :: Maybe Int
-      case paramV of
-        Nothing   -> return 1
-        (Just v') -> return v'
