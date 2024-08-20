bd6dc3867937c53b03b2cccb0f4dd0a42a575e5a
mrtstg <russiankurwacsgo@gmail.com>
2024-08-20 10:46:02 +0300

    feat: implemented vm clone methods and payload struct

--- libs/ln2-proxmox-api/src/Api/Proxmox/VM.hs
+++ libs/ln2-proxmox-api/src/Api/Proxmox/VM.hs
@@ -3,6 +3,8 @@
 module Api.Proxmox.VM
   ( getNodeVMs
   , getNodeVMs'
+  , cloneVM
+  , cloneVM'
   ) where
 
 import           Api
@@ -10,11 +12,32 @@ import           Api.Proxmox
 import           Control.Monad.Trans.Except
 import qualified Data.ByteString.Char8             as BS
 import           Data.Models.Proxmox.API.VM
+import           Data.Models.Proxmox.API.VMClone
 import           Data.Models.Proxmox.Configuration
 import qualified Data.Text                         as T
 import           Network.HTTP.Simple
 import           Network.HTTP.Types.Status
 import           Yesod.Core                        (liftIO)
+
+cloneVM' :: ProxmoxConfiguration -> VMCloneParams -> IO (Either String ())
+cloneVM' conf payload = commonHttpErrorHandler $ cloneVM conf payload
+
+cloneVM :: ProxmoxConfiguration -> VMCloneParams -> ExceptT HttpException IO (Either String ())
+cloneVM conf@(ProxmoxConfiguration { .. }) payload@(VMCloneParams { .. }) = do
+  let reqString = T.unpack $ "POST " <> proxmoxBaseUrl <> "/nodes/" <> proxmoxNodeName <> "/qemu/" <> (T.pack . show) getVMCloneVMID <> "/clone"
+  request' <- parseRequest reqString
+  request <- liftIO $ prepareProxmoxRequest conf request'
+  let jsonRequest = setRequestBodyJSON payload request
+  response <- httpJSONEither jsonRequest :: ExceptT HttpException IO (Response (Either JSONException (ProxmoxResponseWrapper String)))
+  let status = getResponseStatus response
+  let body = getResponseBody response
+  case body of
+    (Left e) -> (return . Left) $ show e
+    (Right (ProxmoxResponseWrapper _ errors)) -> do
+      if statusIsSuccessful status then return (Right ()) else do
+        case errors of
+          Nothing        -> (return . Left) $ errorTextFromStatus status
+          (Just errors') -> (return . Left) $ show errors'
 
 getNodeVMs' :: ProxmoxConfiguration -> IO (Either String [ProxmoxVM])
 getNodeVMs' conf@(ProxmoxConfiguration { proxmoxNodeName = nodeName }) = commonHttpErrorHandler $ getNodeVMs conf nodeName
@@ -30,6 +53,6 @@ getNodeVMs conf@(ProxmoxConfiguration { .. }) nodeName = do
     200 -> do
       let body = getResponseBody response
       case body of
-        (Left e)                             -> (return . Left) $ show e
-        (Right (ProxmoxResponseWrapper vms)) -> return $ Right vms
+        (Left e)                               -> (return . Left) $ show e
+        (Right (ProxmoxResponseWrapper vms _)) -> return $ Right vms
     _someError -> (return . Left) $ show (statusCode status) <> BS.unpack (statusMessage status)
---
+++ libs/ln2-proxmox-api/src/Data/Models/Proxmox/API/VMClone.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+module Data.Models.Proxmox.API.VMClone
+  ( VMCloneParams(..)
+  ) where
+
+import           Data.Aeson
+import           Data.Text  (Text)
+
+-- TODO: implement all fields: https://pve.proxmox.com/pve-docs/api-viewer/#/nodes/{node}/qemu/{vmid}/clone
+data VMCloneParams = VMCloneParams
+  { getVMCloneNewID       :: !Int
+  , getVMCloneNode        :: !Text -- field is omitted when decoding to JSON
+  , getVMCloneVMID        :: !Int -- field is omitted when decoding to JSON
+  , getVMCloneDescription :: !(Maybe Text)
+  , getVMCloneName        :: !(Maybe Text)
+  , getVMCloneSnapname    :: !(Maybe Text)
+  } deriving Show
+
+instance ToJSON VMCloneParams where
+  toJSON (VMCloneParams { .. }) = object $
+    [ "newid" .= getVMCloneNewID ]
+    ++ vmName
+    ++ vmSnapname
+    ++ vmDescription where
+      vmName = case getVMCloneName of
+        Nothing        -> []
+        (Just vmName') -> [ "name" .= vmName' ]
+      vmSnapname = case getVMCloneSnapname of
+        Nothing            -> []
+        (Just vmSnapname') -> [ "snapname" .= vmSnapname' ]
+      vmDescription = case getVMCloneDescription of
+        Nothing               -> []
+        (Just vmDescription') -> [ "description" .= vmDescription' ]
