ca6be46b167673270d27ef8946fe234ec183e9d3
mrtstg <russiankurwacsgo@gmail.com>
2024-08-26 20:10:28 +0300

    feat: added functions for generating network names

--- proxmox-deploy-api/src/Crud/Network.hs
+++ proxmox-deploy-api/src/Crud/Network.hs
@@ -1,10 +1,14 @@
+{-# LANGUAGE RecordWildCards #-}
 module Crud.Network
-  ( suggestVMName
+  ( suggestNetworkName
+  , generateNetworkNamesMap
   ) where
 
 import           Api.Proxmox.SDNNetwork
+import qualified Data.Map                           as M
 import           Data.Models.Proxmox.API.SDNNetwork
 import           Data.Models.Proxmox.Configuration
+import           Data.Models.Proxmox.Deploy.Network
 import           Utils.IO
 import           Utils.Random                       (randomIOString)
 
@@ -14,11 +18,22 @@ generateName' networks = do
   if networkName `elem` networks then return (Left "Network name repeat") else do
     return (Right networkName)
 
-suggestVMName :: ProxmoxConfiguration -> IO (Either String String)
-suggestVMName conf = do
+suggestNetworkName :: ProxmoxConfiguration -> IO (Either String String)
+suggestNetworkName conf = do
   networks' <- retryIOEither' $ getSDNNetworks' conf
   case networks' of
     (Left e) -> return (Left e)
     (Right networks) -> do
       let takenNames = map getSDNNetworkName networks
       retryIOEither 10 0 (generateName' takenNames)
+
+generateNetworkNamesMap :: ProxmoxConfiguration -> [DeployNetwork] -> IO (Either String NetworkNameReplaceMap)
+generateNetworkNamesMap conf = helper M.empty where
+  helper :: NetworkNameReplaceMap -> [DeployNetwork] -> IO (Either String NetworkNameReplaceMap)
+  helper acc []                                = (pure . pure) acc
+  helper acc ((DeployNetwork { .. }):networks) = do
+    suggestedName <- suggestNetworkName conf
+    case suggestedName of
+      (Left e) -> (return . Left) e
+      (Right networkName) -> do
+        helper (M.insert getDeployNetworkName networkName acc) networks
