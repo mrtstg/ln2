113a60ebf972ca23f2d975c17e7ff97dd9e603d2
mrtstg <russiankurwacsgo@gmail.com>
2024-09-04 20:34:08 +0300

    feat: added methods for getting deployments by user

---
+++ libs/ln2-proxmox-api/src/Api/Deploy/User.hs
@@ -0,0 +1,33 @@
+module Api.Deploy.User
+  ( getUserDeployments
+  , getUserDeployments'
+  ) where
+
+import           Api
+import           Control.Monad.Trans.Except
+import qualified Data.ByteString.Char8      as BS
+import           Data.Functor               ((<&>))
+import           Data.Models.Deployment
+import           Data.Models.Endpoints
+import           Network.HTTP.Simple
+import           Network.HTTP.Types.Status
+
+type Page = Int
+type UserId = Int
+
+getUserDeployments :: EndpointsConfiguration -> Page -> UserId -> IO (Either String (ApiPageWrapper [Deployment]))
+getUserDeployments endpoints pageN userId = commonHttpErrorHandler $ getUserDeployments' endpoints pageN userId
+
+getUserDeployments' :: EndpointsConfiguration -> Page -> UserId -> ExceptT HttpException IO (Either String (ApiPageWrapper [Deployment]))
+getUserDeployments' (EndpointsConfiguration { getVMDeployAPIUrl = Just apiUrl }) pageN userId = do
+  let reqString = "GET " <> apiUrl <> "/deployments/user/" <> show userId
+  request <- parseRequest reqString
+    <&> setRequestQueryString [(BS.pack "page", (Just . BS.pack) (show pageN))]
+  response <- httpJSONEither request
+  let status = getResponseStatus response
+  if statusIsSuccessful status then do
+    case getResponseBody response of
+      (Left e)    -> (return . Left . show) e
+      (Right res) -> (return . Right) res
+  else (return . Left) $ errorTextFromStatus status
+getUserDeployments' _ _ _ = return (Left "VM API url is not defined")
