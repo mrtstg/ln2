7d10d1906832cb82e3d858ee723814a47b9dbf42
mrtstg <russiankurwacsgo@gmail.com>
2024-09-06 10:33:56 +0300

    feat: added function for displaying deployment status

--- frontend/api/utils/vnc.ts
+++ frontend/api/utils/vnc.ts
@@ -1,3 +1,5 @@
+import type { DeploymentStatus } from "../types/deployment"
+
 // note: generates relative link by default
 export const generateVNCLink = (vmid: string): string => {
   return "/vm/" + vmid + "/vnc"
@@ -5,4 +7,23 @@ export const generateVNCLink = (vmid: string): string => {
 
 export const generateVNCConsoleLink = (vmid: string): string => {
   return "/vm/" + vmid + "/console"
+}
+
+export const vncStatusToString = (status: DeploymentStatus): string => {
+  switch (status) {
+    case 'queued':
+      return "В очереди"
+    case 'created':
+      return "Создано"
+    case 'deleted':
+      return "Удалено"
+    case 'creating':
+      return "Создается"
+    case 'deleting':
+      return "Удаляется"
+    case 'createError':
+      return "Ошибка создания"
+    case 'deleteError':
+      return 'Ошибка удаления'
+  }
 }
