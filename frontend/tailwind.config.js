4d73e2e107d4e4602e9ac0433fc2cdd2705d40d5
mrtstg <russiankurwacsgo@gmail.com>
2024-09-07 15:41:39 +0300

    feat: added tailwind read for layed in components

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/src/*.{html,svelte,js,ts}", "./components/*.svelte"],
+  content: ["./**/src/*.{html,svelte,js,ts}", "./components/*.svelte", "./components/**/*.svelte"],
   theme: {
     extend: {},
   },
