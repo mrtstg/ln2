f848ae611ae53a5bc8e5fb3c8922b357694806b2
mrtstg <russiankurwacsgo@gmail.com>
2024-09-07 21:05:36 +0300

    refactor: simplified vm connection page

--- main-site/src/Handlers/VMConsole.hs
+++ main-site/src/Handlers/VMConsole.hs
@@ -11,10 +11,22 @@ import           Yesod.Core
 getVMConsoleR :: Int -> Handler Html
 getVMConsoleR _ = do
   _ <- requireAuth
-  defaultLayout $ do
-    setTitle "Подключение к VM"
+  pc <- widgetToPageContent $ do
+    setTitle "VM"
     [whamlet|
 <div .container.pt-2.py-3>
-  <div #app>
+  <div .h-max.w-max #app>
 <script src="/static/js/vmPage.js">
+|]
+  withUrlRenderer [hamlet|
+$doctype 5
+<html>
+  <head>
+    <title> #{pageTitle pc}
+    <meta charset=utf-8>
+    ^{pageHead pc}
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel=stylesheet href=/static/css/styles.css>
+  <body .vmbody>
+    ^{pageBody pc}
 |]
