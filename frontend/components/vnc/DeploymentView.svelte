eb2ed8e97a96d6f619fec1bd92bb6bdbe3c0112d
mrtstg <russiankurwacsgo@gmail.com>
2024-09-08 21:06:48 +0300

    refactor: redesign of connection tab

--- frontend/components/vnc/DeploymentView.svelte
+++ frontend/components/vnc/DeploymentView.svelte
@@ -2,53 +2,26 @@
   import type { DeploymentRead } from "../../api/types/deployment"
   import { generateVNCConsoleLink, vncStatusToString, generateVNCLink } from "../../api/utils/vnc"
   import NoVnc from "./NoVNC.svelte"
+  import NewTabIcon from "../../components/icons/NewTab.svelte"
 
   export let showDesktopName: boolean = true
   export let deploymentData: DeploymentRead
   export let wsHost: string
   export let wsProto: string
 
-  const getMessageColor = (): string => {
-    switch(deploymentData.status) {
-      case "queued":
-        return 'is-dark'
-      case "creating":
-        return 'is-dark'
-      case "created":
-        return 'is-success'
-      case "createError":
-        return 'is-danger'
-      case "deleting":
-        return 'is-dark'
-      case "deleteError":
-        return 'is-danger'
-      case "deleted":
-        return 'is-dark'
-    }
-  }
-
-  const connectVM = (vmId: string) => {
-    if (!connectedVMs.includes(vmId)) {
-      connectedVMs = [vmId, ...connectedVMs]
+  const getTabStyle = (isActive: boolean): string => {
+    if (isActive) {
+      return "border-gray-900 text-gray-900 dark:text-white dark:border-white"
     } else {
-      connectedVMs = connectedVMs.filter(el => el != vmId)
-    }
-  }
-
-  const getConnectButtonStyle = (vmId: string): string => {
-    if (connectedVMs.includes(vmId)) {
-      return 'bg-red-400'
-    } else {
-      return 'bg-green-400'
+      return "text-gray-500 border-gray-500"
     }
   }
 
   const getVNCLink = (vmId: string): string => wsProto + "://" + wsHost + generateVNCLink(vmId)
-  let connectedVMs: string[]
-  $: connectedVMs = []
+  let activeVM: string = ''
 </script>
 
-<article class="message {getMessageColor()} is-fullwidth">
+<article class="message is-fullwidth">
   {#if deploymentData.courseName != null && deploymentData.taskName != null }
     <div class="message-header">
       <div class="flex flex-row justify-between items-center w-full">
@@ -61,15 +34,18 @@
     </div>
   {/if}
   <div class="message-body">
-    {#each Object.entries(deploymentData.vmMap) as [key, value]}
-      <div class="p-3 bg-slate-400 text-slate-200 flex flex-row justify-between items-center">
-        <p> { key } </p>
-        <div class="flex flex-row">
-          <button class="icon large { getConnectButtonStyle(value) }" on:click={() => connectVM(value)}> O </button>
-          <a class="icon large bg-teal-400" target="_blank" href={generateVNCConsoleLink(value)}> N </a>
+    <div class="font-semibold text-neutral-950 dark:text-neutral-200 flex flex-row flex-wrap items-center">
+      {#each Object.entries(deploymentData.vmMap) as [key, value]}
+        <div class="pl-2 flex items-center flex-row border-b-2 cursor-pointer {getTabStyle(key == activeVM)}">
+          <span on:click={() => { activeVM = (activeVM == key) ? "" : key }}> { key } </span>
+          <a class="icon my-1" target="_blank" href={generateVNCConsoleLink(value)}>
+            <NewTabIcon/>
+          </a>
         </div>
-      </div>
-      {#if connectedVMs.includes(value) }
+      {/each}
+    </div>
+    {#each Object.entries(deploymentData.vmMap) as [key, value]}
+      {#if activeVM == key }
         <NoVnc url={getVNCLink(value)} {showDesktopName}/>
       {/if}
     {/each}
