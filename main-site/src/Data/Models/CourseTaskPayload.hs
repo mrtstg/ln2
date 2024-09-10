3b4823d0b73edd3969c0900981df006e93d8189e
mrtstg <russiankurwacsgo@gmail.com>
2024-09-10 20:59:12 +0300

    feat: added course task payload structure

---
+++ main-site/src/Data/Models/CourseTaskPayload.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+module Data.Models.CourseTaskPayload
+  ( CourseTaskPayload(..)
+  , CourseTaskType(..)
+  , courseTaskTypeFromString
+  ) where
+
+import           Data.Aeson
+import qualified Data.Aeson.KeyMap                  as K
+import           Data.Models.Proxmox.Deploy.Network (DeployNetwork)
+import           Data.Models.Proxmox.Deploy.VM      (DeployVM)
+import           Data.Models.StandCheck
+import           Data.Text                          (Text)
+
+data CourseTaskType = ContainerTask | VMTask deriving (Eq, Enum)
+
+instance Show CourseTaskType where
+  show ContainerTask = "contaner"
+  show VMTask        = "vm"
+
+courseTaskTypeFromString :: String -> Maybe CourseTaskType
+courseTaskTypeFromString "container" = Just ContainerTask
+courseTaskTypeFromString "vm"        = Just VMTask
+courseTaskTypeFromString _           = Nothing
+
+instance FromJSON CourseTaskType where
+  parseJSON = withText "CourseTaskType" $ \case
+    "container" -> pure ContainerTask
+    "vm" -> pure VMTask
+    _ -> fail "Unknown type"
+
+instance ToJSON CourseTaskType where
+  toJSON VMTask        = String "vm"
+  toJSON ContainerTask = String "container"
+
+data CourseTaskPayload = ContainerTaskPayload
+  { getPayloadContainerActions :: ![StandCheckStage]
+  , getPayloadStandIdentifier  :: !Text
+  }
+  | VMTaskPayload
+  { getPayloadVMs      :: ![DeployVM]
+  , getPayloadNetworks :: ![DeployNetwork]
+  }
+
+instance ToJSON CourseTaskPayload where
+  toJSON (VMTaskPayload { .. }) = object
+    [ "type" .= String "vm"
+    , "vms" .= getPayloadVMs
+    , "networks" .= getPayloadNetworks
+    ]
+  toJSON (ContainerTaskPayload { .. }) = object
+    [ "type" .= String "container"
+    , "actions" .= getPayloadContainerActions
+    , "standIdentifier" .= getPayloadStandIdentifier
+    ]
+
+instance FromJSON CourseTaskPayload where
+  parseJSON = withObject "CourseTaskPayload" $ \v -> case K.lookup "type" v of
+    Just (String "vm")        -> VMTaskPayload
+      <$> v .: "vms"
+      <*> v .: "networks"
+    Just (String "container") -> ContainerTaskPayload
+      <$> v .: "actions"
+      <*> v .: "standIdentifier"
+    _otherKey                 -> fail "Unknown payload type"
