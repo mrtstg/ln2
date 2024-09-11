616ae8b0cc559c666d1dd07c07bcd732b4b52d31
mrtstg <russiankurwacsgo@gmail.com>
2024-09-11 15:34:44 +0300

    feat: now solve card tracks and updates its status if task is not finished

--- frontend/components/SolveCard.svelte
+++ frontend/components/SolveCard.svelte
@@ -1,14 +1,55 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import { ApiClient } from "../api/client"
-  import type { CourseTaskSolve, TaskResult, TaskResultWrapper } from "../api/types"
+  import type { CourseTaskSolve, TaskResult, TaskStatus, TaskResultWrapper } from "../api/types"
   import { taskStatusToString } from "../api/utils"
 
   export let apiUrl: string;
   export let solveData: CourseTaskSolve
   export let selectCallback: (task: CourseTaskSolve) => Promise<void>
 
+  let solveDetails: TaskResultWrapper | null = null
+  let invervalID: number | null = null
+
+  const taskDetailsWrapper = async (id: string): Promise<TaskResultWrapper | null> => {
+    const res = await api.getTaskDetails(id)
+    if (res != null) {
+      solveDetails = res
+
+      if (
+          invervalID == null && 
+          (res.status != 'error' 
+            && res.status != 'timeout' 
+            && res.status != 'finished' 
+            && res.status != 'accepted' 
+            && res.status != 'cancelled')
+        ) {
+          invervalID = setInterval(() => {
+            detailsCallback = taskDetailsWrapper(solveData.id)
+          }, 1500)
+        }
+
+      if (
+        invervalID != null &&
+        (res.status != 'queued' && res.status != 'taken' && res.status != 'processing')
+      ) {
+        clearInterval(invervalID)
+      }
+    }
+
+    return res
+  }
+
+  onMount(() => {
+    return () => {
+      if (invervalID != null) {
+        clearInterval(invervalID)
+      }
+    }
+  })
+
   const api = new ApiClient(apiUrl)
-  const detailsCallback: Promise<TaskResultWrapper | null> = api.getTaskDetails(solveData.id)
+  let detailsCallback: Promise<TaskResultWrapper | null> = taskDetailsWrapper(solveData.id)
 </script>
 
 <div class="card" on:click={() => selectCallback(solveData)}>
@@ -19,22 +60,23 @@
       <p class="card-header-title"> Решение {solveData.id} </p>
     {/if}
   </header>
-  {#await detailsCallback}
-  {:then res}
-    <div class="card-content">
-      {#if res != null}
-        <p> Статус: { taskStatusToString(res.status) } </p>
-        {#if res.result != null}
-          <p>{ res.result.score } / { res.result.scoreGate } баллов</p>
-          {#if res.result.accepted }
-            <p class="text-green-500"> Решение зачтено </p>
-          {/if}
-        {:else}
-          Итогов проверки нет
+  <div class="card-content">
+    {#await detailsCallback}
+    {/await}
+    {#if solveDetails != null}
+      <p> Статус: { taskStatusToString(solveDetails.status) } </p>
+      {#if solveDetails.result != null}
+        {#if solveDetails.result.score != 0 && solveDetails.result.scoreGate != 0}
+          <p>{ solveDetails.result.score } / { solveDetails.result.scoreGate } баллов</p>
+        {/if}
+        {#if solveDetails.result.accepted }
+          <p class="text-green-500"> Решение зачтено </p>
         {/if}
       {:else}
-        <p> Данные проверки не представлены </p>
+        Итогов проверки нет
       {/if}
-    </div>
-  {/await}
+    {:else}
+      <p> Данные проверки не представлены </p>
+    {/if}
+  </div>
 </div>
