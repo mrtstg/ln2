c5408c1b43f1c882149892033b78ca0d7d480d67
mrtstg <russiankurwacsgo@gmail.com>
2024-09-12 20:42:22 +0300

    feat: specified vm deploy types

---
+++ frontend/api/types/vm.ts
@@ -0,0 +1,40 @@
+export type VM = TemplateVM
+
+// TODO: add omitted fields
+export type TemplateVM = {
+  template: string,
+  name: string,
+  sockets: number | null,
+  cores: number | null,
+  memory: number | null,
+  networks: Array<VMNetworkInterface>
+}
+
+export type VMNetworkInterface = {
+  type: NetworkDeviceType,
+  bridge: string,
+  firewall: boolean
+}
+
+export type VMNetwork = {
+  name: string
+}
+
+export const allNetworkDevicesTypes = [
+  "e1000",
+  "e1000-82540em",
+  "e1000-82544gc",
+  "e1000-82545em",
+  "e1000e" ,
+  "i82551" ,
+  "i82557b",
+  "i82559er",
+  "ne2k_isa",
+  "ne2k_pci",
+  "pcnet",
+  "rtl8139",
+  "virtio" ,
+  "vmxnet3"
+]
+
+export type NetworkDeviceType = typeof allNetworkDevicesTypes[number]
