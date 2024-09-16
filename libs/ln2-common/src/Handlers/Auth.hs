209f4e384afc149368eb570d91b378e823fcb922
mrtstg <russiankurwacsgo@gmail.com>
2024-09-16 19:59:09 +0300

    feat: added function for checking only user auth

--- libs/ln2-common/src/Handlers/Auth.hs
+++ libs/ln2-common/src/Handlers/Auth.hs
@@ -3,11 +3,14 @@ module Handlers.Auth
   ( requireApiAuth
   , api403Error
   , checkAuth
+  , checkUserAuth
   ) where
 
 import           Api.Auth
 import           Data.Aeson
-import           Data.Models.Endpoints (EndpointsConfiguration)
+import           Data.Models.Auth
+import           Data.Models.Auth.Token
+import           Data.Models.Endpoints  (EndpointsConfiguration)
 import           Data.Models.User
 import           Network.HTTP.Types
 import           Yesod.Core
@@ -15,18 +18,25 @@ import           Yesod.Core
 api403Error :: Value
 api403Error = object [ "error" .= String "Unauthorized!" ]
 
-requireApiAuth :: EndpointsConfiguration -> HandlerFor a UserDetails
+requireApiAuth :: EndpointsConfiguration -> HandlerFor a AuthSource
 requireApiAuth endpoints = do
+  authRes <- checkAuth endpoints
+  case authRes of
+    Nothing        -> sendStatusJSON status403 api403Error
+    (Just authSrc) -> return authSrc
+
+checkUserAuth :: EndpointsConfiguration -> HandlerFor a (Maybe UserDetails)
+checkUserAuth endpoints = do
   tokenValue' <- lookupCookie "session"
   case tokenValue' of
-    Nothing -> sendStatusJSON status403 api403Error
+    Nothing -> return Nothing
     (Just tokenValue) -> do
       validRes <- liftIO $ validateToken' endpoints tokenValue
       case validRes of
-        (Left _)     -> sendStatusJSON status403 api403Error
-        (Right resp) -> return resp
+        (Left _)     -> return Nothing
+        (Right resp) -> (return . Just) resp
 
-checkAuth :: EndpointsConfiguration -> HandlerFor a (Maybe UserDetails)
+checkAuth :: EndpointsConfiguration -> HandlerFor a (Maybe AuthSource)
 checkAuth endpoints = do
   tokenValue' <- lookupCookie "session"
   case tokenValue' of
@@ -34,9 +44,13 @@ checkAuth endpoints = do
       authHeader' <- lookupBearerAuth
       case authHeader' of
         Nothing            -> return Nothing
-        (Just bearerToken) -> undefined
+        (Just bearerToken) -> do
+          validRes <- liftIO $ validateJWTToken' endpoints bearerToken
+          case validRes of
+            (Left _)     -> return Nothing
+            (Right resp) -> (return . Just . TokenAuth) resp
     (Just tokenValue) -> do
       validRes <- liftIO $ validateToken' endpoints tokenValue
       case validRes of
         (Left _)     -> return Nothing
-        (Right resp) -> return $ Just resp
+        (Right resp) -> (return . Just . UserAuth) resp
