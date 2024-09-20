f676c5f2c405bce1eeadb6782ed56511b99d52b8
mrtstg <russiankurwacsgo@gmail.com>
2024-09-20 19:39:53 +0300

    feat: added function for creating read-only errors

---
+++ frontend/api/utils/deployment.ts
@@ -0,0 +1,32 @@
+import type { DeploymentErrorKind } from "../types/deployment"
+
+export const deploymentErrorToString = (data: [DeploymentErrorKind, any]): string => {
+  let kind = data[0]
+  let body = data[1]
+  switch(kind) {
+  case 'template':
+    return 'Неизвестный шаблон: ' + body.value
+  case 'missingNetwork':
+    return 'Неизвестная сеть: ' + body.value
+  case 'forbiddenNetwork':
+    return 'Недопустимое название сети: ' + body.value
+  case 'emptyVMName':
+    return 'Заполните все названия виртуальных машин'
+  case 'emptyNetworkName':
+    return 'Заполните все названия сетей'
+  case 'longVMName':
+    return 'Названия всех виртуальных машин должны быть короче 16 символов!'
+  case 'longNetworkName':
+    return 'Названия всех сетей должны быть короче 16 символов!'
+  case 'invalidCPU':
+    return 'Некорректное значение CPU: ' + body.value
+  case 'invalidSockets':
+    return 'Некорректное значение сокета: ' + body.value
+  case 'invalidMemory':
+    return 'Некорректное значение памяти: ' + body.value
+  case 'unknown':
+		return 'Неизвестная ошибка'
+  default:
+		return 'Неизвестная ошибка'
+  }
+}
