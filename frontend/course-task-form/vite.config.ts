29d5827c244b45925c158a451637e4edfcbba0f8
mrtstg <russiankurwacsgo@gmail.com>
2024-10-03 22:11:06 +0300

    chore: added new build variables

--- frontend/course-task-form/vite.config.ts
+++ frontend/course-task-form/vite.config.ts
@@ -5,7 +5,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig(({ command, mode }) => {
   return {
     define: {
-      API_URL: JSON.stringify(command === 'serve' ? "http://localhost" : "")
+      WS_URL: JSON.stringify(command === 'serve' ? "localhost" : ""),
+      API_URL: JSON.stringify(command === 'serve' ? "http://localhost" : ""),
+      WS_PROTO: JSON.stringify(command === 'serve' ? "ws" : "wss"),
+      DEV_MODE: JSON.stringify(command === 'serve' ? "1" : "0")
     },
     plugins: [svelte()],
     build: {
