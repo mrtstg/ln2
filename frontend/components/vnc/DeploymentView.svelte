e521994b49429d088b42906359ed45fe1e4b4791
mrtstg <russiankurwacsgo@gmail.com>
2024-10-06 21:16:47 +0300

    feat: added destroy stand button

--- frontend/components/vnc/DeploymentView.svelte
+++ frontend/components/vnc/DeploymentView.svelte
@@ -1,19 +1,42 @@
 <script lang="ts">
+  import { ApiClient } from "../../api/client"
   import type { DeploymentRead } from "../../api/types/deployment"
   import { generateVNCConsoleLink, vncStatusToString, generateVNCLink } from "../../api/utils/vnc"
   import NoVnc from "./NoVNC.svelte"
   import NewTabIcon from "../../components/icons/NewTab.svelte"
 
+  let callDelete: boolean = false
+  let waitForDelete: boolean = false
+
   export let showDesktopName: boolean = true
+  export let showDestroyButton: boolean = true
   export let deploymentData: DeploymentRead
   export let wsHost: string
   export let wsProto: string
+  export let apiUrl: string
+
+  const api = new ApiClient(apiUrl)
 
   const getTabStyle = (isActive: boolean): string => {
     if (isActive) {
       return "border-gray-900 text-gray-900 dark:text-white dark:border-white"
     } else {
       return "text-gray-500 border-gray-500"
+    }
+  }
+
+  const deleteDeploymentWrapper = async () => {
+    if (callDelete) {
+      const res = await api.deleteDeployment(deploymentData.id)
+    } else {
+      waitForDelete = true
+      setTimeout(() => {
+        callDelete = true
+        waitForDelete = false
+        setTimeout(() => {
+          callDelete = false
+        }, 5000)
+      }, 15000)
     }
   }
 
@@ -47,6 +70,18 @@
           </div>
         {/each}
       </div>
+      <br>
+      {#if showDestroyButton}
+        <button disabled={waitForDelete} class="button is-danger">
+          {#if callDelete}
+            Удалить стенд
+          {:else if waitForDelete}
+            Данное действие сотрет все действия! Ожидайте 15 секунд для подтверждения
+          {:else}
+            Удалить стенд (окончательно!)
+          {/if}
+        </button>
+      {/if}
       {#each Object.entries(deploymentData.vmMap) as [key, value]}
         {#if activeVM == key }
           <NoVnc url={getVNCLink(value)} {showDesktopName}/>
