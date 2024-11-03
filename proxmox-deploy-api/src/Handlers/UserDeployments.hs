2fa120f6a042e46bc370f63f1820da4050cb7eee
mrtstg <russiankurwacsgo@gmail.com>
2024-11-03 21:37:01 +0300

    feat: added flag for hiding user unavailable VMs in user endpoints request

--- proxmox-deploy-api/src/Handlers/UserDeployments.hs
+++ proxmox-deploy-api/src/Handlers/UserDeployments.hs
@@ -3,6 +3,7 @@ module Handlers.UserDeployments
   ( getUserDeploymentsR
   ) where
 
+import           Data.Maybe
 import qualified Data.Text          as T
 import           Database.Persist
 import           Foundation
@@ -17,12 +18,14 @@ import           Yesod.Persist
 getUserDeploymentsR :: Int -> Handler Value
 getUserDeploymentsR userId = do
   _ <- requireServiceAuth' requireApiAuth
+  hideFlag <- lookupGetParam "showUnavailable"
+  let showHidden = fromMaybe "1" hideFlag == "1"
   let pageSize = 50
   pageN <- getPageNumber
   let params = [LimitTo pageSize, OffsetBy $ (pageN - 1) * pageSize]
   deploymentsAmount <- runDB $ count [ MachineDeploymentUserId ==. userId ]
   deployments <- runDB $ selectList [ MachineDeploymentUserId ==. userId ] params
-  let convertRes = traverse (toMachineDeploymentRead True) deployments
+  let convertRes = traverse (toMachineDeploymentRead showHidden) deployments
   case convertRes of
     (Left e) -> sendStatusJSON status500 $ object [ "error" .= T.pack ("Parse error: " <> e)]
     (Right objects) -> do
