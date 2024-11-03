06106de3bf35de1e4a3b6d31e7354b927184e030
mrtstg <russiankurwacsgo@gmail.com>
2024-11-03 21:40:38 +0300

    feat: added support for showUnavaiable flag in getUserDeployments

--- libs/ln2-proxmox-api/src/Api/Deploy/User.hs
+++ libs/ln2-proxmox-api/src/Api/Deploy/User.hs
@@ -14,16 +14,17 @@ import           Network.HTTP.Types.Status
 
 type Page = Int
 type UserId = Int
+type ShowAllVM = Bool
 
-getUserDeployments' :: EndpointsConfiguration -> Page -> UserId -> IO (Either String (ApiPageWrapper [Deployment]))
-getUserDeployments' endpoints pageN userId = commonHttpErrorHandler $ getUserDeployments endpoints pageN userId
+getUserDeployments' :: EndpointsConfiguration -> Page -> UserId -> ShowAllVM -> IO (Either String (ApiPageWrapper [Deployment]))
+getUserDeployments' endpoints pageN userId showHidden = commonHttpErrorHandler $ getUserDeployments endpoints pageN userId showHidden
 
-getUserDeployments :: EndpointsConfiguration -> Page -> UserId -> ExceptT HttpException IO (Either String (ApiPageWrapper [Deployment]))
-getUserDeployments (EndpointsConfiguration { getVMDeployAPIUrl = Just apiUrl, getEndpointsAccessToken = token }) pageN userId = do
+getUserDeployments :: EndpointsConfiguration -> Page -> UserId -> ShowAllVM -> ExceptT HttpException IO (Either String (ApiPageWrapper [Deployment]))
+getUserDeployments (EndpointsConfiguration { getVMDeployAPIUrl = Just apiUrl, getEndpointsAccessToken = token }) pageN userId showHidden = do
   let reqString = "GET " <> apiUrl <> "/deployments/user/" <> show userId
   request <- parseRequest reqString
     <&> prepareCommonRequest token
-    . setRequestQueryString [(BS.pack "page", (Just . BS.pack) (show pageN))]
+    . setRequestQueryString [(BS.pack "page", (Just . BS.pack) (show pageN)), (BS.pack "showUnavailable", (Just . BS.pack) (if showHidden then "1" else "0"))]
   response <- httpJSONEither request
   let status = getResponseStatus response
   if statusIsSuccessful status then do
@@ -31,4 +32,4 @@ getUserDeployments (EndpointsConfiguration { getVMDeployAPIUrl = Just apiUrl, ge
       (Left e)    -> (return . Left . show) e
       (Right res) -> (return . Right) res
   else (return . Left) $ errorTextFromStatus status
-getUserDeployments _ _ _ = return (Left "VM API url is not defined")
+getUserDeployments _ _ _ _ = return (Left "VM API url is not defined")
