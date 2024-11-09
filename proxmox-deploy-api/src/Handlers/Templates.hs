7941f6790936266ff3b00bf8658cba8f0000964d
mrtstg <russiankurwacsgo@gmail.com>
2024-11-09 17:59:47 +0300

    refactor+feat: used new validation method, unified errors, added error type field

--- proxmox-deploy-api/src/Handlers/Templates.hs
+++ proxmox-deploy-api/src/Handlers/Templates.hs
@@ -22,6 +22,20 @@ import           Utils
 import           Yesod.Core
 import           Yesod.Persist
 
+templateNameTakenError :: Value
+templateNameTakenError = object [ "error" .= String "Template name is taken", "type" .= String "templateNameTaken"]
+
+templateIDTakenError :: Value
+templateIDTakenError = object [ "error" .= String "Template ID is taken", "type" .= String "templateIDTaken" ]
+
+newTemplateExists :: Maybe Int -> Handler Bool
+newTemplateExists Nothing = return False
+newTemplateExists (Just tid) = runDB $ exists [ MachineTemplateProxmoxId ==. tid ]
+
+newNameTaken :: Maybe Text -> Handler Bool
+newNameTaken Nothing     = return False
+newNameTaken (Just name) = runDB $ exists [ MachineTemplateName ==. name ]
+
 getTemplatesR :: Handler Value
 getTemplatesR = do
   () <- requireAnyAuth' requireApiAuth
@@ -39,14 +53,22 @@ postTemplatesR :: Handler Value
 postTemplatesR = let
   f :: MachineTemplateCreate -> Handler (Status, Value)
   f payload@(MachineTemplateCreate { .. }) = do
-    templateExists <- runDB $ exists [ MachineTemplateProxmoxId ==. getTemplateCreateId ]
-    if templateExists then return (status400, object [ "error" .= String "Template exists" ]) else do
-      template' <- runDB $ do
-        key <- insert (machineTemplateCreateToModel payload)
-        get key
-      case template' of
-        Nothing -> return (status500, object [ "error" .= String "Something went wrong" ])
-        (Just template) -> return (status200, toJSON $ machineTemplateFromModel template)
+    idTaken <- newTemplateExists (Just getTemplateCreateId)
+    nameTaken <- newNameTaken (Just getTemplateCreateName)
+    case (idTaken, nameTaken) of
+      (True, _)     -> return (status400, templateIDTakenError)
+      (False, True) -> return (status400, templateNameTakenError)
+      (False, False) -> do
+        let validationResult = httpCheckTemplatePatch (machineTemplateCreateToPatch payload)
+        case validationResult of
+          Just resp -> sendCurryJSON resp
+          Nothing -> do
+            template' <- runDB $ do
+              key <- insert (machineTemplateCreateToModel payload)
+              get key
+            case template' of
+              Nothing -> return (status500, object [ "error" .= String "Something went wrong" ])
+              (Just template) -> return (status200, toJSON $ machineTemplateFromModel template)
   in do
   () <- requireAdminOrServiceAuth' requireApiAuth
   payload <- requireCheckJsonBody
@@ -54,35 +76,31 @@ postTemplatesR = let
 
 patchTemplateR :: Int -> Handler Value
 patchTemplateR oldTemplateID = let
-  newTemplateExists :: Maybe Int -> Handler Bool
-  newTemplateExists Nothing = return False
-  newTemplateExists (Just tid) = runDB $ exists [ MachineTemplateProxmoxId ==. tid ]
-
-  newNameTaken :: Maybe Text -> Handler Bool
-  newNameTaken Nothing     = return False
-  newNameTaken (Just name) = runDB $ exists [ MachineTemplateName ==. name ]
-
   f :: MachineTemplatePatch -> Handler (Status, Value)
   f p@(MachineTemplatePatch { .. }) = do
     oldTemplateExists <- runDB $ exists [ MachineTemplateProxmoxId ==. oldTemplateID ]
-    if not oldTemplateExists then return (status404, object [ "error" .= String "Not found" ]) else do
+    if not oldTemplateExists then return (status404, object [ "error" .= String "Not found", "type" .= String "templateNotFound" ]) else do
       newTemplateExists' <- newTemplateExists getTemplatePatchId
       newNameTaken' <- newNameTaken getTemplatePatchName
       case (newTemplateExists', newNameTaken') of
-        (True, _) -> return (status400, object [ "error" .= String "New ID is taken" ])
-        (False, True) -> return (status400, object [ "error" .= String "New name is taken"])
+        (True, _) -> return (status400, templateIDTakenError)
+        (False, True) -> return (status400, templateNameTakenError)
         (False, False) -> do
           case templatePatchToQuery p of
-            [] -> return (status400, object [ "error" .= String "No update body"])
+            [] -> return (status400, object [ "error" .= String "No update body", "type" .= String "noUpdateBody"])
             q -> do
-              template' <- runDB $ do
-                updateWhere [ MachineTemplateProxmoxId ==. oldTemplateID ] q
-                case getTemplatePatchId of
-                  (Just newId) -> selectFirst [ MachineTemplateProxmoxId ==. newId ] []
-                  Nothing -> selectFirst [ MachineTemplateProxmoxId ==. oldTemplateID ] []
-              case template' of
-                Nothing -> return (status500, object [ "error" .= String "Something went wrong" ])
-                (Just (Entity _ template)) -> return (status200, toJSON $ machineTemplateFromModel template)
+              let validationResult = httpCheckTemplatePatch p
+              case validationResult of
+                Just resp -> sendCurryJSON resp
+                Nothing -> do
+                  template' <- runDB $ do
+                    updateWhere [ MachineTemplateProxmoxId ==. oldTemplateID ] q
+                    case getTemplatePatchId of
+                      (Just newId) -> selectFirst [ MachineTemplateProxmoxId ==. newId ] []
+                      Nothing -> selectFirst [ MachineTemplateProxmoxId ==. oldTemplateID ] []
+                  case template' of
+                    Nothing -> return (status500, object [ "error" .= String "Something went wrong" ])
+                    (Just (Entity _ template)) -> return (status200, toJSON $ machineTemplateFromModel template)
   in do
   () <- requireAdminOrServiceAuth' requireApiAuth
   payload <- requireCheckJsonBody
@@ -93,7 +111,7 @@ deleteTemplateR templateId = let
   f :: Handler (Status, Value)
   f = do
     templateExists <- runDB $ exists [ MachineTemplateProxmoxId ==. templateId ]
-    if not templateExists then return (status404, object [ "error" .= String "Not found" ]) else do
+    if not templateExists then return (status404, object [ "error" .= String "Not found", "type" .= String "templateNotFound" ]) else do
       runDB $ deleteWhere [ MachineTemplateProxmoxId ==. templateId ]
       return (status204, object [])
   in do
