5e045d77a3d2138ec40c34d73bec4f1b287e8ce4
mrtstg <russiankurwacsgo@gmail.com>
2024-11-19 13:08:04 +0300

    refactor: simplified bearer token check function

--- proxmox-fs-agent/src/Handlers/Utils.hs
+++ proxmox-fs-agent/src/Handlers/Utils.hs
@@ -2,18 +2,16 @@
 module Handlers.Utils (checkToken) where
 
 import           Data.Aeson
-import           Data.Text.Encoding
 import           Foundation
 import           Network.HTTP.Types
 import           Yesod.Core
 
 checkToken :: Handler ()
 checkToken = do
-  headerValue <- lookupHeader "Authorization"
+  headerValue <- lookupBearerAuth
   case headerValue of
     Nothing -> sendStatusJSON status401 $ object ["error" .= String "Unauthorized"]
     (Just v) -> do
-      let stringV = decodeUtf8 v
       App { accessToken = tokenValue } <- getYesod
-      if stringV == "Bearer " <> tokenValue then return () else do
+      if v == tokenValue then return () else do
         sendStatusJSON status401 $ object ["error" .= String "Unauthorized"]
