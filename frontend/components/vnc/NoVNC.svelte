6dc500c817f43f2b36f69a7df840d3ca721c69f1
mrtstg <russiankurwacsgo@gmail.com>
2024-11-26 22:03:05 +0300

    feat: added handling of vm disconnect and connection event

--- frontend/components/vnc/NoVNC.svelte
+++ frontend/components/vnc/NoVNC.svelte
@@ -5,6 +5,7 @@
   import { NoVncOptions } from "@novnc/novnc/lib/rfb";
 
   let desktopName = ""
+  let connected: boolean = false
   let quality: number = 7
   let parent: HTMLElement;
   let container: HTMLElement;
@@ -12,9 +13,15 @@
 
   export let showDesktopName: boolean = true
   export let url: string;
+  export let onConnectCallback: () => Promise<void> = async () => {}
+  export let onDisconnectCallback: () => Promise<void> = async () => {}
 
   const updateDesktop = (e: CustomEvent<{ name: string }>) => {
     desktopName = e.detail.name;
+  }
+
+  const updateConnect = (state: boolean) => {
+    connected = state
   }
 
   const changeQuality = (delta: number) => {
@@ -33,6 +40,8 @@
     rfb.scaleViewport = true
 
     rfb.addEventListener("desktopname", updateDesktop)
+    rfb.addEventListener("connect", (_) => { updateConnect(true); onConnectCallback() })
+    rfb.addEventListener("disconnect", (_) => { updateConnect(false); onDisconnectCallback() })
 
     return () => {
       rfb?.disconnect()
@@ -42,8 +51,12 @@
 
 <div class="vnc-container__wrapper">
   <div class="vnc-container">
-    <div class="flex flex-row items-center justify-between dark:text-slate-200 text-slate-900">
-      <h3 class="text-xl font-semibold"> { showDesktopName ? desktopName : "" } </h3>
+    <div class="flex flex-row items-center justify-between dark:text-slate-200 text-slate-900 w-full">
+      {#if showDesktopName}
+        <h3 class="text-xl font-semibold"> { showDesktopName ? (desktopName.length == 0 ? "Виртуальная машина" : desktopName) : "" } - { connected ? "подключено" : "отключено" } </h3>
+      {:else}
+        <h3 class="text font-semibold"> { connected ? "Подключено" : "Отключено" } </h3>
+      {/if}
     </div>
     <div class="w-max-content h-max-content" bind:this={container}>
       <div class="vnc-screen-container" bind:this={parent}></div>
