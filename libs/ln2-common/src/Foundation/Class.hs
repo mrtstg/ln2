e47a7a2a713cf8e107a313ab09cbd435dce7e62b
mrtstg <russiankurwacsgo@gmail.com>
2025-01-10 18:26:04 +0300

    feat: added new foundation classes for proxmox app

--- libs/ln2-common/src/Foundation/Class.hs
+++ libs/ln2-common/src/Foundation/Class.hs
@@ -1,6 +1,8 @@
 module Foundation.Class
   ( EndpointsApp(..)
   , AuthBypassApp(..)
+  , StartDisplayApp(..)
+  , StartVMIDApp(..)
   ) where
 
 import           Data.Models.Endpoints
@@ -10,3 +12,10 @@ class EndpointsApp a where
 
 class AuthBypassApp a where
   appAuthBypass :: a -> Bool
+
+-- used in proxmox
+class StartDisplayApp a where
+  startDisplay :: a -> Int
+
+class StartVMIDApp a where
+  startVMID :: a -> Int
